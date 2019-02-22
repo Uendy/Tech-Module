b31cc15335504d59e03889bda10fac6b600930f4
Uendy <alex.beshkov98@gmail.com>
2019-02-22 19:38:24 +0200

    Began working on L02 Data Types and Variables/L02 Other Exercises/L02 Other Exercises.sln Did Q07 and Q08

---
+++ L02 Data Types and Variables/L02 Other Exercises/Q07 Water Overflow/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        //You have a water tank with capacity of 255 liters.
+        //On the next n lines, you will receive liters of water, which you have to pour in your tank. 
+        //If the capacity is not enough, print “Insufficient capacity!” and continue reading the next line. 
+        //On the last line, print the liters in the tank.
+        int numberOfInputs = int.Parse(Console.ReadLine());
+
+        int capacity = 255;
+        int currentlyOccupied = 0;
+
+        for (int indexOfInput = 0; indexOfInput < numberOfInputs; indexOfInput++)
+        {
+            int currentBatch = int.Parse(Console.ReadLine());
+            if (currentBatch > capacity || currentBatch + currentlyOccupied > capacity)
+            {
+                Console.WriteLine("Insufficient capacity!");
+                continue;
+            }
+            else
+            {
+                currentlyOccupied += currentBatch;
+            }
+        }
+
+        Console.WriteLine(currentlyOccupied);
+    }
+}
---
+++ L02 Data Types and Variables/L02 Other Exercises/Q08 Beer Kegs/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //Write a program, which calculates the volume of n beer kegs. 
+        //You will receive in total 3 * n lines. Each three lines will hold information for a single keg. 
+        //First up is the model of the keg, after that is the radius of the keg, and lastly is the height of the keg.
+        //Calculate the volume using the following formula: π* r^ 2 * h.
+        //At the end, print the model of the biggest keg.
+
+        int numberOfKegs = int.Parse(Console.ReadLine());
+
+        var dictOfKegs = new Dictionary<string, double>();
+
+        for (int indexOfKeg = 0; indexOfKeg < numberOfKegs; indexOfKeg++)
+        {
+            string model = Console.ReadLine();
+
+            decimal radius = decimal.Parse(Console.ReadLine());
+
+            int height = int.Parse(Console.ReadLine());
+
+            double volume = Math.PI * Math.Pow((double)radius, 2) * height;
+
+            dictOfKegs[model] = volume;
+        }
+
+        var resultDict = dictOfKegs.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value).Keys.First();
+        Console.WriteLine(resultDict);
+
+    }
+}
+
