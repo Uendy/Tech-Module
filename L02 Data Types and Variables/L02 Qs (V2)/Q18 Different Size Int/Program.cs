fa41058a22989c3f3102e6d61e079d860f3d6270
Uendy <alex.beshkov98@gmail.com>
2019-02-22 18:36:19 +0200

    Finished up Qs (18 & 19)

---
+++ L02 Data Types and Variables/L02 Qs (V2)/Q18 Different Size Int/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //Given an input integer, you must determine which primitive data types are capable of properly storing that input.
+        // my method would be much better if I could do a recursion, but I don't know how to do it with data types ex <T> number = <T>.Parse...
+        string input = Console.ReadLine();
+        var firstChar = input.ToCharArray().First();
+
+        if (firstChar == '-') // its a negative (signed)
+        {
+            var listOfSignedDataTypes = new List<string>(new string[] { "sbyte", "short", "int", "long" });
+
+            try
+            {
+                sbyte number = sbyte.Parse(input);
+                Console.WriteLine($"{number} can fit in:");
+                PrintUnsignedDataTypes(listOfSignedDataTypes);
+            }
+            catch (Exception)
+            {
+                listOfSignedDataTypes.Remove("sbyte");
+                try
+                {
+                    short number = short.Parse(input);
+                    Console.WriteLine($"{number} can fit in:");
+                    PrintUnsignedDataTypes(listOfSignedDataTypes);
+                }
+                catch (Exception)
+                {
+                    listOfSignedDataTypes.Remove("short");
+                    try
+                    {
+                        int number = int.Parse(input);
+                        Console.WriteLine($"{number} can fit in:");
+                        PrintUnsignedDataTypes(listOfSignedDataTypes);
+                    }
+                    catch (Exception)
+                    {
+                        listOfSignedDataTypes.Remove("int");
+                        try
+                        {
+                            long number = long.Parse(input);
+                            Console.WriteLine($"{number} can fit in:");
+                            PrintUnsignedDataTypes(listOfSignedDataTypes);
+
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine($"{input} Can't fit in any type");
+                        }
+                    }
+                }
+            }
+        }
+        else // any integer Data Type
+        {
+            var listOfDataTypes = new List<string>(new string[] {"sbyte", "byte", "short", "ushort", "int", "uint", "long" });
+
+            try
+            {
+                sbyte number = sbyte.Parse(input);
+                Console.WriteLine($"{number} can fit in:");
+                PrintAllDataTypes(listOfDataTypes);
+            }
+            catch (Exception)
+            {
+                listOfDataTypes.Remove("sbyte");
+                try
+                {
+                    byte number = byte.Parse(input);
+                    Console.WriteLine($"{number} can fit in:");
+                    PrintAllDataTypes(listOfDataTypes);
+                }
+                catch (Exception)
+                {
+                    listOfDataTypes.Remove("byte");
+                    try
+                    {
+                        short number = short.Parse(input);
+                        Console.WriteLine($"{number} can fit in:");
+                        PrintAllDataTypes(listOfDataTypes);
+                    }
+                    catch (Exception)
+                    {
+                        listOfDataTypes.Remove("short");
+                        try
+                        {
+                            ushort number = ushort.Parse(input);
+                            Console.WriteLine($"{number} can fit in:");
+                            PrintAllDataTypes(listOfDataTypes);
+                        }
+                        catch (Exception)
+                        {
+                            listOfDataTypes.Remove("ushort");
+                            try
+                            {
+                                int number = int.Parse(input);
+                                Console.WriteLine($"{number} can fit in:");
+                                PrintAllDataTypes(listOfDataTypes);
+                            }
+                            catch (Exception)
+                            {
+                                listOfDataTypes.Remove("int");
+                                try
+                                {
+                                    uint number = uint.Parse(input);
+                                    Console.WriteLine($"{number} can fit in:");
+                                    PrintAllDataTypes(listOfDataTypes);
+                                }
+                                catch (Exception)
+                                {
+                                    listOfDataTypes.Remove("uint");
+                                    try
+                                    {
+                                        long number = long.Parse(input);
+                                        Console.WriteLine($"{number} can fit in:");
+                                        PrintAllDataTypes(listOfDataTypes);
+                                    }
+                                    catch (Exception)
+                                    {
+                                        listOfDataTypes.Remove("long");
+                                        try
+                                        {
+                                            ulong number = ulong.Parse(input);
+                                            Console.WriteLine($"{number} can fit in:");
+                                            PrintAllDataTypes(listOfDataTypes);
+                                        }
+                                        catch (Exception)
+                                        {
+                                            Console.WriteLine($"{input} can't fit in any type");
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    public static void PrintUnsignedDataTypes(List<string> listOfUnsignedDataTypes)
+    {
+        foreach (var dataType in listOfUnsignedDataTypes)
+        {
+            Console.WriteLine($"* {dataType}");
+        }
+    }
+
+    public static void PrintAllDataTypes(List<string> listOfDataTypes)
+    {
+        foreach (var dataType in listOfDataTypes)
+        {
+            Console.WriteLine($"* {dataType}");
+        }
+    }
+}
---
+++ L02 Data Types and Variables/L02 Qs (V2)/Q19 Thea the Photographer/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        //On the first line you will receive an integer N – the amount of pictures Thea had taken.
+        //On the second line you will receive an integer FT – the amount of time(filter time) in seconds that Thea will require to filter every single picture.
+        //On the third line you will receive an integer FF – the filter factor or the percentage of the total pictures that are considered “good” to be uploaded.
+        //On the fourth line you will receive an integer UT – the amount of time needed for every filtered picture to be uploaded to her storage.
+
+        long totalPictures = long.Parse(Console.ReadLine());
+        int singlePictureFilter = int.Parse(Console.ReadLine());
+        sbyte percentageOfGoodPics = sbyte.Parse(Console.ReadLine());
+        long singleUploadTimeForPics = long.Parse(Console.ReadLine());
+
+        long totalTime = totalPictures * singlePictureFilter;
+
+        double leftoverPics = Math.Ceiling((double)totalPictures * ((double)percentageOfGoodPics / 100));
+
+        totalTime += (long)leftoverPics * singleUploadTimeForPics;
+
+        long daysInSeconds = 60 * 60 * 24; //84000
+        long days = totalTime / daysInSeconds;
+        if (days >= 1)
+        {
+            totalTime -= daysInSeconds * days;
+        }
+
+        long hoursInSeconds = 60 * 60; //3600
+        long hours = totalTime / hoursInSeconds;
+        if (hours >= 1)
+        {
+            totalTime -= hoursInSeconds * hours;
+        }
+
+        long minutesInSeconds = 60;
+        long minutes = totalTime / minutesInSeconds;
+        if (minutes >= 1)
+        {
+            totalTime -= minutesInSeconds * minutes;
+        }
+
+        long seconds = totalTime; // or just put totaltime as {3} instead of seconds
+
+        Console.WriteLine($"{days}:{hours:D2}:{minutes:D2}:{seconds:D2}");
+
+    }
+}
