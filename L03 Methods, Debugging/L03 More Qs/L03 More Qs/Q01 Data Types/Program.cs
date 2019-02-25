cfb9a3d990ef8f20073eff4624e145c40abe78db
Uendy <alex.beshkov98@gmail.com>
2019-02-25 19:45:54 +0200

    Adding Q01 Data Types/Program.cs of L03 More Qs.sln

---
+++ L03 Methods, Debugging/L03 More Qs/L03 More Qs/Q01 Data Types/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string dataType = Console.ReadLine();
+
+        switch (dataType)
+        {
+            case "int":
+                int inputNumber = int.Parse(Console.ReadLine());
+                Console.WriteLine(IntModifier(inputNumber));
+                break;
+
+            case "real":
+                double inputDecimal = double.Parse(Console.ReadLine());
+                inputDecimal = DoubleModifier(inputDecimal);
+                inputDecimal.ToString();
+                Console.WriteLine($"{inputDecimal:f2}");
+                break;
+
+            case "string":
+                string inputString = Console.ReadLine();
+                Console.WriteLine(StringModifier(inputString));
+                break;
+        }
+    }
+
+    static int IntModifier(int inputNumber)
+    {
+        return inputNumber * 2;
+    }
+
+    static double DoubleModifier (double inputDecimal)
+    {
+        inputDecimal *= 1.5;
+        return Math.Round(inputDecimal, 2);
+    }
+
+    static string StringModifier(string inputString)
+    {
+        inputString = '$' + inputString;
+        inputString = inputString + '$';
+        return inputString;
+    }
+}
