5063e3fdb700a758e89312c8a183c225f6659144
Uendy <alex.beshkov98@gmail.com>
2019-03-01 22:04:21 +0200

    Adding Q09 of L03 New Methods Qs/L03 New Qs/L03 New Qs.sln

---
+++ L03 Methods, Debugging/L03 New Methods Qs/L03 New Qs/Q09 Palendrome Int/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        //A palindrome is a number which reads the same backward as forward, such as 323 or 1001.
+        //Write a program which reads a positive integer numbers until you receive "End",
+        //for each number print whether the number is palindrome or not.
+
+        string input = Console.ReadLine();
+        while (input != "END") 
+        {
+            Console.WriteLine(CheckIfPalendrome(input).ToString().ToLower());
+
+            input = Console.ReadLine();
+        } 
+    }
+
+    public static bool CheckIfPalendrome(string input)
+    {
+        bool isPalendrome = false;
+
+        if (input.Length == 1)
+        {
+            return isPalendrome = true;
+        }
+        else  // odd length
+        {
+            var inputAsCharArr = input.ToCharArray();
+
+            var firstHalf = string.Empty;
+            for (int index = 0; index < inputAsCharArr.Length / 2; index++)
+            {
+                firstHalf += inputAsCharArr[index];
+            }
+
+            var secondHalf = string.Empty;
+            for (int index = inputAsCharArr.Length - 1; index >= inputAsCharArr.Length / 2; index--)
+            {
+                secondHalf += inputAsCharArr[index];
+            }
+
+            if (firstHalf == secondHalf)
+            {
+                return isPalendrome = true;
+            }
+        }
+
+        return isPalendrome;
+    }
+}
