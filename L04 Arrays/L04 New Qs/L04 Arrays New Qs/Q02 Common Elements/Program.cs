e02017a1f3b2ebd99f0059aa044f1ecade8d1ee2
Uendy <alex.beshkov98@gmail.com>
2019-03-08 20:19:47 +0200

    Adding Q02 Common Elements/Program.cs

---
+++ L04 Arrays/L04 New Qs/L04 Arrays New Qs/Q02 Common Elements/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //Write a program, which prints common elements in two arrays.
+        //You have to compare the elements of the second array to the elements of the first.
+
+        var firstArrString = Console.ReadLine();
+        var firstArray = firstArrString.Split(' ');
+
+        var secondArrString = Console.ReadLine();
+        var secondArray = secondArrString.Split(' ');
+
+        string commonElements = string.Empty;
+
+        foreach (var currentString in firstArray)
+        {
+            bool commonElement = secondArray.Contains(currentString);
+            if (commonElement == true)
+            {
+                commonElements = string.Concat(commonElements, currentString + ' ');
+            }
+        }
+
+        var commonEsArr = commonElements.Split(' ');
+        var output = string.Join(" ", commonEsArr.Reverse());
+
+        Console.WriteLine(output);
+    }
+}
