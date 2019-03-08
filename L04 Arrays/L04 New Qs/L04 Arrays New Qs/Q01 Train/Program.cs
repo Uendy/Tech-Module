65dea67b432482aeaeb7c785d1ad624f2a085ca3
Uendy <alex.beshkov98@gmail.com>
2019-03-08 20:07:42 +0200

    Adding L04 Arrays/L04 New Qs/L04. Arrays-New-Exercises.docx Adding Q01 Train/Program.cs

---
+++ L04 Arrays/L04 New Qs/L04 Arrays New Qs/Q01 Train/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Check any questions here: https://judge.softuni.bg/Contests/Practice/Index/1206#0
+
+        //You will be given a count of wagons in a train n. 
+        //On the next n lines you will receive how many people are going to get on each wagon.
+        //At the end print the whole train and after that the sum of the people in the train. 
+
+        int numberOfInputs = int.Parse(Console.ReadLine());
+        int[] arrayOfWagons = new int[numberOfInputs];
+
+        int sum = 0;
+
+        for (int index = 0; index < numberOfInputs; index++)
+        {
+            int passengers = int.Parse(Console.ReadLine());
+            arrayOfWagons[index] = passengers;
+            sum += passengers;
+        }
+
+        string ouput = string.Join(" ", arrayOfWagons);
+        Console.WriteLine(ouput);
+        Console.WriteLine(sum);
+    }
+}
