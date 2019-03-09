405735206129f42cd05a253473a904a404685653
Uendy <alex.beshkov98@gmail.com>
2019-03-09 23:00:40 +0200

    Adding first draft of Q04 Rotate Array.csproj

---
+++ L04 Arrays/L04 New Qs/L04 Arrays New Qs/Q04 Rotate Array/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //Write a program that receives an array and number of rotations you have to perform 
+        //(first element goes at the end) Print the resulting array.
+
+        var inputAsString = Console.ReadLine();
+        var array = inputAsString.Split(' ').Select(int.Parse).ToArray();
+
+        int numberOfRotations = int.Parse(Console.ReadLine());
+
+        while (numberOfRotations >= array.Count())
+        {
+            numberOfRotations -= array.Count();
+        }
+
+        var temporaryArray = new int[array.Count()];
+
+        int index = 0;
+        foreach (var num in array)
+        {
+            if (index - numberOfRotations < 0)
+            {
+                temporaryArray[index] = array[array.Count() - numberOfRotations - 1 - index];
+            }
+            else
+            {
+                temporaryArray[index] = array[index - numberOfRotations];
+            }
+
+            //array = temporaryArray.ToArray();
+            index++;
+        }
+
+        Console.WriteLine(string.Join(" ", array));
+    }
+}
