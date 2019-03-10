4d216e1eb0e353d6559f7b7895e4875fad67b9e7
Uendy <alex.beshkov98@gmail.com>
2019-03-10 14:32:00 +0200

    Adding Q05 Top Integer.csproj

--- L04 Arrays/L04 New Qs/L04 Arrays New Qs/Q05 Top Integer/Program.cs
+++ L04 Arrays/L04 New Qs/L04 Arrays New Qs/Q05 Top Integer/Program.cs
@@ -1,15 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Q05_Top_Integer
+public class Program
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        //Write a program to find all the top integers in an array.
+        //A top integer is an integer which is bigger than all the elements to its right. 
+
+        var array = Console.ReadLine()
+            .Split(' ').
+            Select(int.Parse)
+            .ToArray();
+
+        var listOfTopNumbers = new List<int>();
+
+        for (int index = 0; index < array.Length; index++)
         {
+            var currentNumber = array[index];
+            bool isTopNumber = true;
+
+            if (index == array.Length - 1)
+            {
+                listOfTopNumbers.Add(currentNumber);
+            }
+            else
+            {
+                for (int i = index + 1; i < array.Length; i++)
+                {
+                    var checkedNumber = array[i];
+
+                    bool theCheck = currentNumber >= checkedNumber;
+                    if (theCheck == false)
+                    {
+                        isTopNumber = false;
+                    }
+                }
+
+                if (isTopNumber == true)
+                {
+                    listOfTopNumbers.Add(currentNumber);
+                }
+            }
         }
+
+        string output = string.Join(" ", listOfTopNumbers);
+        Console.WriteLine(output);
     }
 }
