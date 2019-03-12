a45c63cae85eb7e98f1b93672dbba358cb98293a
Uendy <alex.beshkov98@gmail.com>
2019-03-12 20:23:05 +0200

    Adding Q07 Max Sequence.csproj

---
+++ L04 Arrays/L04 New Qs/L04 Arrays New Qs/Q07 Max Sequence/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //Write a program that finds the longest sequence of equal elements in an array of integers.
+        //If several longest sequences exist, print the leftmost one.
+
+        var array = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToArray();
+
+        var longestLength = 1;
+        int digitOfSequence = int.MinValue;
+
+        for (int index = 0; index < array.Length; index++)
+        {
+            var currentNum = array[index];
+            int innerIndex = index + 1;
+            int currentConsecutiveCount = 1;
+
+            while (innerIndex < array.Length && currentNum == array[innerIndex])
+            {
+                currentConsecutiveCount++;
+
+                bool newLongestSeq = longestLength < currentConsecutiveCount;
+                if(newLongestSeq == true)
+                {
+                    digitOfSequence = currentNum;
+                    longestLength = currentConsecutiveCount;
+                }
+
+                innerIndex++;
+            }
+        }
+
+        string outPut = string.Empty;
+        for (int i = 0; i < longestLength; i++)
+        {
+            outPut += digitOfSequence.ToString() + ' ';
+        }
+
+        Console.WriteLine(outPut);
+    }
+}
