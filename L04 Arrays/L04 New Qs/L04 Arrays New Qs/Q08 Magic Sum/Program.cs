56b64e7e5bc15f79bd23d35a64a3efb57b8973b4
Uendy <alex.beshkov98@gmail.com>
2019-03-13 15:26:50 +0200

    Adding Q08 Magic Sum.csproj

---
+++ L04 Arrays/L04 New Qs/L04 Arrays New Qs/Q08 Magic Sum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //Write a program, which prints all unique pairs in an array of integers,
+        //whose sum is equal to a given number. 
+
+        var array = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToArray();
+
+        int magicNum = int.Parse(Console.ReadLine());
+
+        for (int firstIndex = 0; firstIndex < array.Length - 1; firstIndex++)
+        {
+            for (int secondIndex = firstIndex + 1; secondIndex < array.Length; secondIndex++)
+            {
+                if (array[firstIndex] + array[secondIndex] == magicNum)
+                {
+                    Console.WriteLine($"{array[firstIndex]} {array[secondIndex]}");
+                }
+            }
+        }
+    }
+}
