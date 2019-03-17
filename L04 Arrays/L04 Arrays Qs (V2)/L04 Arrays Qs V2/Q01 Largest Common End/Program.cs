faae2e7442946dd1702d4d23ff1f69e460b88d4b
Uendy <alex.beshkov98@gmail.com>
2019-03-17 21:30:11 +0200

    Adding Q01 V2.csproj

---
+++ L04 Arrays/L04 Arrays Qs (V2)/L04 Arrays Qs V2/Q01 Largest Common End/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //Read two arrays of words and find the length of the largest common end (left or right).
+        var firstArray = Console.ReadLine().Split(' ').ToArray();
+        var secondArray = Console.ReadLine().Split(' ').ToArray();
+
+        bool commonAtStart = firstArray[0] == secondArray[0];
+        if (commonAtStart == true)
+        {
+            CommonCounter(firstArray, secondArray);
+        }
+        else //common at end
+        {
+            firstArray = firstArray.Reverse().ToArray();
+            secondArray = secondArray.Reverse().ToArray();
+            CommonCounter(firstArray, secondArray);
+        }
+    }
+
+    public static void CommonCounter(string[] firstArray, string[] secondArray)
+    {
+        int commonCounter = 0;
+        for (int index = 0; index < firstArray.Length || index < secondArray.Length; index++)
+        {
+            if (firstArray[index] == secondArray[index])
+            {
+                commonCounter++;
+            }
+            else
+            {
+                Console.WriteLine(commonCounter);
+                Environment.Exit(0);
+            }
+        }
+    }
+}
---
+++ L04 Arrays/L04 Arrays Qs (V2)/L04 Arrays Qs V2/Q01 V2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+public class LargestCommonEnd
+{
+    public static void Main()
+    {
+        string[] firstArray = Console.ReadLine().Split().ToArray();
+        string[] secondArray = Console.ReadLine().Split().ToArray();
+        int size = Math.Min(firstArray.Length, secondArray.Length);
+        int length1 = firstArray.Length;
+        int length2 = secondArray.Length;
+        int countLeft = 0, countRight = 0;
+
+        // CHECK LEFT side
+        for (int i = 0; i < size; i++)
+        {
+            if (firstArray[i] == secondArray[i])
+            {
+                countLeft++;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        // CHECK RIGHT side
+        for (int i = 1; i < size; i++)
+        {
+            if (firstArray[length1 - i] == secondArray[length2 - i])
+            {
+                countRight++;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        if (countLeft == 0 && countRight == 0)
+        {
+            Console.WriteLine(countLeft);
+        }
+        else
+        {
+            Console.WriteLine(Math.Max(countLeft, countRight));
+        }
+
+
+    } // End of Main
+}
+}
