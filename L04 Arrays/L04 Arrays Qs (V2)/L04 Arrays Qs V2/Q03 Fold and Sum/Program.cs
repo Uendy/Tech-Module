94f3c85dc9e8bbacdffe79ac56b2a625a87b4ed3
Uendy <alex.beshkov98@gmail.com>
2019-03-19 10:40:28 +0200

    Adding Q03 Fold and Sum.csproj

---
+++ L04 Arrays/L04 Arrays Qs (V2)/L04 Arrays Qs V2/Q03 Fold and Sum/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //Read an array of 4*k integers, fold it like shown below, 
+        //and print the sum of the upper and lower two rows (each holding 2 * k integers):
+
+        var array = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToArray();
+
+        var length = array.Length / 2;
+
+        var firstHalf = array
+            .Take(length)
+            .ToArray();
+        var secondHalf = array
+            .Skip(length)
+            .Take(length)
+            .ToArray();
+
+        int halfArrayLength = length / 2;
+
+        var firstArrayOutPut = new int[halfArrayLength]; // firstArray
+        firstArrayOutPut = FoldAndSum(firstHalf, halfArrayLength);
+        firstArrayOutPut = firstArrayOutPut.Reverse().ToArray(); // since it returns it the wrong way around
+
+        var secondArrayOutPut = new int[halfArrayLength]; // secondArray
+        secondArrayOutPut = FoldAndSum(secondHalf, halfArrayLength);
+        
+        var outPutArray = firstArrayOutPut.Concat(secondArrayOutPut);
+        string outPut = string.Join(" ", outPutArray);
+        Console.WriteLine(outPut);
+
+    }
+
+    public static int[] FoldAndSum(int[] firstHalf, int halfArrayLength) ////Folds and Sums the array
+    {
+        int[] foldedAndSummedArray = new int[halfArrayLength];
+
+        for (int index = 0; index < halfArrayLength; index++) 
+        {
+            int rightSideIndex = halfArrayLength * 2 - 1 - index;
+            foldedAndSummedArray[index] = firstHalf[index] + firstHalf[rightSideIndex];
+        }
+
+        return foldedAndSummedArray;
+    }
+}
