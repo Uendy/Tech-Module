84d09cae9a29d158f9ab357724e42597aa2a7d63
Uendy <alex.beshkov98@gmail.com>
2019-03-20 21:02:04 +0200

    Adding Q04 Sieve of Eratosthes.csproj

---
+++ L04 Arrays/L04 Arrays Qs (V2)/L04 Arrays Qs V2/Q04 Sieve of Eratosthes/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //Write a program to find all prime numbers in range [1…n]. Implement the algorithm called “Sieve of Eratosthenes”: https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes. Steps in the “Sieve of Eratosthenes” algorithm:
+        // 1.Assign primes[0…n] = true
+        // 2.Assign primes[0] = primes[1] = false
+        // 3.Find the smallest p, which holds primes[p] = true
+        // •	Print p(it is prime)
+        // •	Assign primes[2 * p] = primes[3 * p] = primes[4 * p] = … = false
+        // 4.Repeat for the next smallest p < n.
+
+        int maxNum = int.Parse(Console.ReadLine());
+        var initialPrimes = new List<int> { 2, 3, 5, 7 };
+
+        if (maxNum <= 7)
+        {
+            foreach (var prime in initialPrimes)
+            {
+                if (maxNum >= prime)
+                {
+                    Console.Write(prime + " ");
+                }
+            }
+            Environment.Exit(0);
+        }
+
+        var list = new List<int>();
+        for (int i = 10; i <= maxNum; i++) 
+        {
+            list.Add(i);
+        }
+
+        foreach (var prime in initialPrimes) // the actual program to weed out any non-primes
+        {
+            var currentPrime = prime;
+            while (currentPrime <= maxNum)
+            {
+                if (list.Contains(currentPrime))
+                {
+                    list.Remove(currentPrime);
+                }
+                currentPrime += prime;
+            }
+        }
+
+        initialPrimes = initialPrimes.Concat(list).ToList();
+        var outPut = string.Join(" ", initialPrimes);
+        Console.WriteLine(outPut);
+    }
+}
