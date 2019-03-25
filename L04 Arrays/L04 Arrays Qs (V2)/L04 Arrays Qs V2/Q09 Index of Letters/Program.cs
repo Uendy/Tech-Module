c35ec9c13fb2aeb44bc3fa52fee69203def1ba7b
Uendy <alex.beshkov98@gmail.com>
2019-03-25 18:11:55 +0200

    Adding the Text of the Question (Q09 Index of Letters/Program.cs)

--- L04 Arrays/L04 Arrays Qs (V2)/L04 Arrays Qs V2/Q09 Index of Letters/Program.cs
+++ L04 Arrays/L04 Arrays Qs (V2)/L04 Arrays Qs V2/Q09 Index of Letters/Program.cs
@@ -3,6 +3,8 @@ public class Program
 {
     public static void Main()
     {
+        //Write a program that creates an array containing all letters from the alphabet (a-z).
+        //Read a lowercase word from the console and print the index of each of its letters in the letters array.
         string input = Console.ReadLine();
         var inputAsArray = input.ToCharArray();
 
