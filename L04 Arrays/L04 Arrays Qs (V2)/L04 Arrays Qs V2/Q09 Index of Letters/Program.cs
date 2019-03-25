8172f404bbcfef528f68d31f192e3947b94f839d
Uendy <alex.beshkov98@gmail.com>
2019-03-25 17:55:47 +0200

    Adding Q09 Index of Letters/Program.cs

---
+++ L04 Arrays/L04 Arrays Qs (V2)/L04 Arrays Qs V2/Q09 Index of Letters/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        string input = Console.ReadLine();
+        var inputAsArray = input.ToCharArray();
+
+        foreach (var letter in inputAsArray)
+        {
+            int letterAsIndex = letter - 'a'; 
+            Console.WriteLine($"{letter} -> {letterAsIndex}");
+        }
+    }
+}
