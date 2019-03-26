40b03a0394c82c6fa6b64f93e6c2a0fd025ca96c
Uendy <alex.beshkov98@gmail.com>
2019-03-26 23:42:41 +0200

    Adding Q01 Rmv Neg and Rev.csproj of L05 Lab Exercise (V2)/05 Lists-Lab.docx

---
+++ L05 Lists/L05 Lab Exercise (V2)/List Lab Qs (v2)/Q01 Rmv Neg and Rev/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //Read a list of integers, remove all negative numbers from it and print the remaining elements in reversed order.
+        //In case of no elements left in the list, print “empty”.
+
+        var list = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+        var finalList = new List<int>();
+
+        foreach (var num in list)
+        {
+            bool notNegative = num >= 0;
+            if (notNegative == true)
+            {
+                finalList.Add(num);
+            }
+        }
+
+        bool emptyList = finalList.Count() == 0;
+        if (emptyList == true)
+        {
+            Console.WriteLine("empty");
+        }
+        else
+        {
+            finalList.Reverse();
+            string outPut = string.Join(" ", finalList);
+            Console.WriteLine(outPut);
+        }
+    }
+}
