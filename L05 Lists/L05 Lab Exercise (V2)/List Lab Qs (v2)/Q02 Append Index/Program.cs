41ebe4ce2a2e7721597a771d2afdef000dd13bdf
Uendy <alex.beshkov98@gmail.com>
2019-03-27 18:32:27 +0200

    Adding Q03 and L05 new Lab Exercise/05. Lists-Lab.docx

---
+++ L05 Lists/L05 Lab Exercise (V2)/List Lab Qs (v2)/Q02 Append Index/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Write a program to append several lists of numbers.
+        //	Lists are separated by ‘|’.
+        //	Values are separated by spaces(‘ ’, one or several)
+        //	Order the lists from the last to the first, and their values from left to right.
+
+        var input = Console.ReadLine();
+        var list = new List<int>();
+
+        var array = input.Split('|')
+            .ToArray();
+        foreach (var setOfNums in array)
+        {
+            var currentSet = setOfNums.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            list.InsertRange(0, currentSet);
+        }
+
+        string outPut = string.Join(" ", list);
+        Console.WriteLine(outPut);
+    }
+}
