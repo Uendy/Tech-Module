2ed1180a01fbfe950c6053428e612f47e694d28a
Uendy <alex.beshkov98@gmail.com>
2019-04-03 21:03:11 +0300

    Adding Q05 Sort Numbers.csproj

---
+++ L05 Lists/L05 Lab Exercise (V2)/List Lab Qs (v2)/Q05 Sort Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //Read a list of decimal numbers and sort them in increasing order.
+        //Print the output as shown in the examples below.
+
+        var array = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+
+        array = array.OrderBy(x => x).ToArray();
+
+        string outPut = string.Join(" <= ", array);
+        Console.WriteLine(outPut);
+    }
+}
