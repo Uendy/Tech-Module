1b5c62dc2f9c2e022245144cf48cd4e2f50da740
Uendy <alex.beshkov98@gmail.com>
2019-04-07 15:56:20 +0300

    Adding Q02 Gauss Trick.csproj

---
+++ L05 Lists/L05 new Lab Exercise/List New Lab Qs/Q02 Gauss Trick/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //Write a program that sums all of the numbers in a list in the following order: 
+        //first + last, first + 1 + last - 1, first + 2 + last - 2, … first + n, last - n.
+
+        var list = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+        var outPutList = new List<int>();
+
+        for (int index = 0; index < list.Count() / 2; index++)
+        {
+            int indexOnRight = list.Count() - 1 - index;
+
+            outPutList.Add(list[index] + list[indexOnRight]);
+        }
+
+        if (list.Count() % 2 != 0) // odd
+        {
+            outPutList.Add(list[list.Count() / 2]);
+        }
+
+        string outPut = string.Join(" ", outPutList);
+        Console.WriteLine(outPut);
+    }
+}
