c84930e2ffab9c4d03a69313b3ebdcbb847b6e57
Uendy <alex.beshkov98@gmail.com>
2019-04-08 15:43:38 +0300

    Adding Q04 List of Products.csproj

---
+++ L05 Lists/L05 new Lab Exercise/List New Lab Qs/Q04 List of Products/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //Read a number n and n lines of products. Print a numbered list of all the products ordered by name.
+
+        int numberOfInputs = int.Parse(Console.ReadLine());
+
+        var listOfProducts = new List<string>();
+
+        for (int i = 0; i < numberOfInputs; i++)
+        {
+            listOfProducts.Add(Console.ReadLine());
+        }
+
+        listOfProducts.Sort();
+        for (int index = 1; index < numberOfInputs + 1; index++)
+        {
+            Console.WriteLine($"{index}.{listOfProducts[index - 1]}");
+        }
+    }
+}
