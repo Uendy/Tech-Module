63f6d77fd740f9e70d0905fadf264c24a31597fd
Uendy <alex.beshkov98@gmail.com>
2019-04-08 15:04:27 +0300

    Adding Q03 Merging Lists.csproj

---
+++ L05 Lists/L05 new Lab Exercise/List New Lab Qs/Q03 Merging Lists/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //You are going to receive two lists with numbers.
+        //Create a result list, which contains the numbers from both of the lists.
+        //The first element should be from the first list, the second from the second list and so on. 
+        //If the length of the two lists are not equal, just add the remaining elements at the end of the list.
+
+        var firstList = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+        var secondList = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+        var outPutList = new List<int>();
+        var shorterCountList = Math.Min(firstList.Count(), secondList.Count());
+
+        for (int index = 0; index < shorterCountList; index++)
+        {
+            outPutList.Add(firstList[index]);
+            outPutList.Add(secondList[index]);
+        }
+
+        if (firstList.Count() > shorterCountList)
+        {
+            outPutList = AddRemainingItems(outPutList, firstList, shorterCountList);
+        }
+        else if (secondList.Count() > shorterCountList)
+        {
+            outPutList = AddRemainingItems(outPutList, secondList, shorterCountList);
+        }
+
+        string outPut = string.Join(" ", outPutList);
+        Console.WriteLine(outPut);
+    }
+
+    public static List<int> AddRemainingItems(List<int> outPutList, List<int> firstList, int shorterCountList)
+    {
+        for (int index = shorterCountList; index < firstList.Count(); index++)
+        {
+            outPutList.Add(firstList[index]);
+        }
+        return outPutList;
+    }
+}
