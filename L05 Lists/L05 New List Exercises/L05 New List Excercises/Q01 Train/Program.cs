0cb419503cf2da43d9c27a28c2555b2f65d22ae5
Uendy <alex.beshkov98@gmail.com>
2019-04-10 11:07:31 +0300

    Adding Q01 Train.csproj

---
+++ L05 Lists/L05 New List Exercises/L05 New List Excercises/Q01 Train/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //You will receive a list of wagons (integers) on the first line. Every integer represents the number of passengers that are currently in each of the wagons. On the next line, you will get the max capacity of each wagon (a single integer). Until you receive "end" you will be given two types of input:
+        //•	Add { passengers} – add a wagon to the end with the given number of passengers.
+        //•	{ passengers} -f ind an existing wagon to fit every passenger, starting from the first wagon.
+        //At the end print the final state of the train(each of the wagons, separated by a space).
+
+        var list = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+        int maxCapacity = int.Parse(Console.ReadLine());
+
+        string input = Console.ReadLine();
+        while (input != "end")
+        {
+            var inputTokens = input.Split(' ').ToList();
+            if (inputTokens[0] == "Add")
+            {
+                int addedWagon = int.Parse(inputTokens[1]);
+                list.Add(addedWagon);
+            }
+            else // more passangers
+            {
+                int numOfPassangers = int.Parse(inputTokens[0]);
+                for (int index = 0; index < list.Count(); index++)
+                {
+                    bool enoughSpace = list[index] + numOfPassangers <= maxCapacity;
+                    if (enoughSpace == true)
+                    {
+                        list[index] += numOfPassangers;
+                        break;
+                    }
+                }
+            }
+
+            input = Console.ReadLine();
+        }
+
+        string outPut = string.Join(" ", list);
+        Console.WriteLine(outPut);
+    }
+}
