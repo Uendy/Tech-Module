7cfa36502370aececd97d9f17a76eda305ee6b22
Uendy <alex.beshkov98@gmail.com>
2019-04-29 13:59:24 +0300

    Adding L06 Dictionaries More Exercises V2/L06 Dictionaries-More-Exercises.docx And Adding Q01 Sort Time.csproj

---
+++ L06 Dictionaries/L06 Dictionaries More Exercises V2/L06 More Exe V2/Q01 Sort Time/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    //Check your solutions here: https://judge.softuni.bg/Contests/582.
+    public static void Main()
+    {
+        //Write a program, which receives a list of times (space-separated, 24-hour format) and sorts them in ascending order.
+        //Print the sorted times comma-separated.
+        //Example: 06:55, 02:30, 23:11 -> 02:30, 06:55, 21:11
+
+        var listOfTimes = Console.ReadLine().Split(' ').ToList();
+        listOfTimes = listOfTimes.OrderBy(x => x).ToList();
+        string outPut = string.Join(", ", listOfTimes);
+        Console.WriteLine(outPut);
+    }
+}
