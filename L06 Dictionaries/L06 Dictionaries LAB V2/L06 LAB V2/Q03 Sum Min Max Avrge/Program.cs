2ab999d58849b73656782e19ea3ee87a52a5f9d6
Uendy <alex.beshkov98@gmail.com>
2019-05-04 18:53:45 +0300

    Adding Q03 Sum Min Max Avrge.csproj

---
+++ L06 Dictionaries/L06 Dictionaries LAB V2/L06 LAB V2/Q03 Sum Min Max Avrge/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //Write a program to read n integers and print their sum, min, max, first, last and average values.
+        int numberOfInputs = int.Parse(Console.ReadLine());
+
+        var listOfNums = new List<int>();
+        for (int i = 0; i < numberOfInputs; i++)
+        {
+            int currentNum = int.Parse(Console.ReadLine());
+            listOfNums.Add(currentNum);
+        }
+
+        int sum = listOfNums.Sum();
+        Console.WriteLine(sum);
+
+        int min = listOfNums.Min();
+        Console.WriteLine(min);
+
+        int max = listOfNums.Max();
+        Console.WriteLine(max);
+
+        double average = listOfNums.Average();
+        Console.WriteLine(average);
+    }
+}
