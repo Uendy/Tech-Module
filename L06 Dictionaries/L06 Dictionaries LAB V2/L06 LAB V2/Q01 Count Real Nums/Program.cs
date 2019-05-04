e68ee87f98693a6dc5d114dccfa6647580dca2d7
Uendy <alex.beshkov98@gmail.com>
2019-05-04 18:17:59 +0300

    Adding L06 Dictionaries LAB V2/L06 LAB V2/L06 LAB V2.sln and Q01 Count Real Nums.csproj

---
+++ L06 Dictionaries/L06 Dictionaries LAB V2/L06 LAB V2/Q01 Count Real Nums/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //Read a list of real numbers and print them in ascending order along with their number of occurrences.
+
+        var numbersAndOccurances = new SortedDictionary<double, int>();
+
+        var input = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+
+        foreach (var number in input)
+        {
+            bool newNumber = !numbersAndOccurances.ContainsKey(number);
+            if (newNumber)
+            {
+                numbersAndOccurances[number] = 1;
+            }
+            else
+            {
+                numbersAndOccurances[number]++;
+            }
+        }
+
+        numbersAndOccurances.OrderByDescending(x => x.Key);
+
+        foreach (var num in numbersAndOccurances.Keys)
+        {
+            Console.WriteLine($"{num} -> {numbersAndOccurances[num]} times");
+        }
+    }
+}
