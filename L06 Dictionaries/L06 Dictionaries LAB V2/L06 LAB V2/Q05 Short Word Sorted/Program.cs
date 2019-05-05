146ad0aae7a2f2c1e3ac9bcf6d7aacc94e11326b
Uendy <alex.beshkov98@gmail.com>
2019-05-05 14:23:53 +0300

    Adding Q05 Short Word Sorted.csproj

---
+++ L06 Dictionaries/L06 Dictionaries LAB V2/L06 LAB V2/Q05 Short Word Sorted/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q05_Short_Word_Sorted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
