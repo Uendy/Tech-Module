bda3ca1ac11543d79554757f49007cbf488da2b0
Uendy <alex.beshkov98@gmail.com>
2019-05-05 14:15:02 +0300

    Adding Q04 Top 3 Num.csproj

---
+++ L06 Dictionaries/L06 Dictionaries LAB V2/L06 LAB V2/Q04 Top 3 Num/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //Read a list of real numbers and print largest 3 of them. 
+        //If less than 3 numbers exit, print all of them.
+
+        var nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+        nums = nums.OrderByDescending(x => x).ToList();
+
+        if (nums.Count() >= 3)
+        {
+            nums = nums.Take(3).ToList();
+        }
+
+        string outPut = string.Join(" ", nums);
+        Console.WriteLine(outPut);
+    }
+}
