250d364c6c8a4efcfb6f9cfa9279fa3a03188788
Uendy <alex.beshkov98@gmail.com>
2019-05-06 18:37:00 +0300

    Adding Q06 Fold And Sum.csproj

---
+++ L06 Dictionaries/L06 Dictionaries LAB V2/L06 LAB V2/Q06 Fold And Sum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //Read an array of 4*k integers, fold it like shown below, and print the sum of the upper and lower rows (2*k integers)
+
+        var arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        int k = arr.Length / 4;
+
+        var rowLeft = arr.Take(k).Reverse();
+        var rowRight = arr.Reverse().Take(k);
+
+        var topRow = rowLeft.Concat(rowRight).ToArray();
+        var botRow = arr.Skip(k).Take(2 * k).ToArray();
+        var sumArr = topRow.Select((x, index) => x + botRow[index]);
+        Console.WriteLine(string.Join(" ", sumArr));
+    }
+}
