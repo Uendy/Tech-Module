8cf7cafc48f055ac4ade3987238ca66015974277
Uendy <alex.beshkov98@gmail.com>
2019-05-07 16:09:57 +0300

    Adding: -L07 Lab V2 folder and word doc of questions -Q01 Day of Week (+ link to test questions)

---
+++ L07 Classes, Objects/L07 Lab V2/L07 Lab Qs V2/Q01 Day of Week/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    //https://judge.softuni.bg/Contests/Practice/Index/175#0 check questions
+    public static void Main()
+    {
+        //You are given a date in format day-month-year. Calculate and print the day of week in English.
+        var input = Console.ReadLine().Split('-').ToArray();
+
+        int day = int.Parse(input[0]);
+        int month = int.Parse( input[1]);
+        int year = int.Parse(input[2]);
+
+        var currentDateTime = new DateTime(year, month, day);
+
+        Console.WriteLine(currentDateTime.DayOfWeek);
+    }
+}
