b9ae2f03ca2af4df5469f67ed43d21f33efd5e1b
Uendy <alex.beshkov98@gmail.com>
2019-05-07 17:11:27 +0300

    Adding Q02 Randomize Words.csproj

---
+++ L07 Classes, Objects/L07 Lab V2/L07 Lab Qs V2/Q02 Randomize Words/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        //You are given a list of words in one line. 
+        //Randomize their order and print each word at a separate line.
+
+        var listOfWords = Console.ReadLine().Split(' ').ToList();
+
+        for (int index = 0; index < listOfWords.Count(); index++)
+        {
+            var random = new Random();
+            int randomNum = random.Next(index, listOfWords.Count());
+            Console.WriteLine(listOfWords[randomNum]);
+        }
+    }
+}
