a338e1726382f7c98edb124a6724477563c343e7
Uendy <alex.beshkov98@gmail.com>
2019-05-07 16:14:09 +0300

    Adding Q01 V2 (solved more directly)

---
+++ L07 Classes, Objects/L07 Lab V2/L07 Lab Qs V2/Q01 V2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+public class Program
+{
+    public static void Main()
+    {
+        //You are given a date in format day-month-year. Calculate and print the day of week in English.
+        var date = Console.ReadLine();
+
+        var currentDateTime = DateTime.ParseExact(date, "d-M-yyyy", CultureInfo.InvariantCulture);
+
+        Console.WriteLine(currentDateTime.DayOfWeek);
+    }
+}
