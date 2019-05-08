9e621d65ac7e5c942a6d4bd086c5a12d782afb5f
Uendy <alex.beshkov98@gmail.com>
2019-05-08 17:22:51 +0300

    Q03 Big Factorial.csproj

---
+++ L07 Classes, Objects/L07 Lab V2/L07 Lab Qs V2/Q03 Big Factorial/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Calculate and print n! (n factorial) for very big integer n (e.g. 1000).
+
+        var num = int.Parse(Console.ReadLine());
+
+        var bigFac = new BigInteger();
+
+        bigFac = num;
+        for (int index = num - 1; index > 0; index--)
+        {
+            bigFac *= index;
+        }
+
+        Console.WriteLine(bigFac);
+    }
+}
