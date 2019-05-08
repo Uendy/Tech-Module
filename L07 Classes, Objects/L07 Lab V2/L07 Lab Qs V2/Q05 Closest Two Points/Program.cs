e3649b9604d4dd1be24a487ced21ed667faa4d2d
Uendy <alex.beshkov98@gmail.com>
2019-05-08 18:39:14 +0300

    Adding Q05 Closest Two Points.csproj

---
+++ L07 Classes, Objects/L07 Lab V2/L07 Lab Qs V2/Q05 Closest Two Points/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //Write a program to read n points and find the closest two of them.
+
+        int numberOfPoints = int.Parse(Console.ReadLine());
+
+        var listOfPoints = new List<Point>();
+
+        // reading all inputs and inserting them into each points properties (x, y), then adding them to list of points
+        for (int index = 0; index < numberOfPoints; index++)
+        {
+            var currentInput = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var currentPoint = new Point();
+            currentPoint.X = currentInput[0];
+            currentPoint.Y = currentInput[1];
+            listOfPoints.Add(currentPoint);
+        }
+
+        // initializing info
+        double smallestDiff = double.MaxValue;
+        int indexOfFirstPoint = 0;
+        int indexOfSecondPoint = 0;
+
+        // comparing the points to eachother in list
+        for (int firstIndex = 0; firstIndex < listOfPoints.Count(); firstIndex++)
+        {
+            var firstPoint = listOfPoints[firstIndex];
+
+            for (int secondIndex = firstIndex + 1; secondIndex < listOfPoints.Count(); secondIndex++)
+            {
+                var secondPoint = listOfPoints[secondIndex];
+
+                //diff calculations
+                int xDifference = Math.Abs(firstPoint.X - secondPoint.X);
+                int yDifference = Math.Abs(firstPoint.Y - secondPoint.Y);
+                double currentDiff = Math.Sqrt(Math.Pow(xDifference, 2) + Math.Pow(yDifference, 2));
+
+                //check if new record small diff
+                bool newSmallest = currentDiff < smallestDiff;
+                if (newSmallest)
+                {
+                    smallestDiff = currentDiff;
+                    indexOfFirstPoint = firstIndex;
+                    indexOfSecondPoint = secondIndex;
+                }
+            }
+
+        }
+
+        // Printing
+        Console.WriteLine($"{smallestDiff:f3}");
+        Console.WriteLine(listOfPoints[indexOfFirstPoint]);
+        Console.WriteLine(listOfPoints[indexOfSecondPoint]);
+    }
+}
