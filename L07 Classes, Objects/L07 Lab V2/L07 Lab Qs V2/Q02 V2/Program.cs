56d18796caf49be8b1819c4afe02b7fe47281586
Uendy <alex.beshkov98@gmail.com>
2019-05-08 17:18:15 +0300

    Adding Q02 V2.csproj

---
+++ L07 Classes, Objects/L07 Lab V2/L07 Lab Qs V2/Q02 V2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+public class Program
+{
+    public static void Main()
+    {
+        //You are given a list of words in one line. 
+        //Randomize their order and print each word at a separate line.
+
+        var words = Console.ReadLine().Split(' ').ToList();
+        var outPutWords = new List<string>();
+
+        while (words.Count() != 0)
+        {
+            var random = new Random();
+            var currentRandom = random.Next(0, words.Count());
+
+            string currentWord = words[currentRandom];
+
+            if (!outPutWords.Contains(currentWord))
+            {
+                words.Remove(currentWord);
+                outPutWords.Add(currentWord);
+            }
+        }
+
+        foreach (var word in outPutWords)
+        {
+            Console.WriteLine(word);
+        }
+
+    }
+}
