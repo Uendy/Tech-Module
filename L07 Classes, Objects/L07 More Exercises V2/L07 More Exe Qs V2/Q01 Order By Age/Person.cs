19d86b4cb9716d66454d5a2bc0f949600156fef8
Uendy <alex.beshkov98@gmail.com>
2019-05-15 17:12:08 +0300

    Adding: - File for L07 Classes, Objects/L07 More Exercises V2/L07 More Exe Qs V2/L07 More Exe Qs V2.sln - Q01 Order By Age.csproj (+ link to check questions)

---
+++ L07 Classes, Objects/L07 More Exercises V2/L07 More Exe Qs V2/Q01 Order By Age/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q01_Order_By_Age
+{
+    class Person
+    {
+    }
+}
