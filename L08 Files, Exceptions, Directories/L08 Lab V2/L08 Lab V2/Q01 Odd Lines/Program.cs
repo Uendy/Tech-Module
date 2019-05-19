c7e111476ba6c35077654196993fc194d413f4ba
Uendy <alex.beshkov98@gmail.com>
2019-05-19 21:25:22 +0300

    Adding L08 Lab V2

---
+++ L08 Files, Exceptions, Directories/L08 Lab V2/L08 Lab V2/Q01 Odd Lines/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+public class Program
+{
+    public static void Main()
+    {
+        //Write a program that reads a text file and writes its every odd line in another file. Line numbers starts from 0. 
+
+        var fileInput = "input.txt";
+
+        var lines = File.ReadAllLines(fileInput);
+
+        var arrayOfOutput = new List<string>();
+
+        for (int indexOfLine = 1; indexOfLine < fileInput.Length; indexOfLine += 2)
+        {
+            arrayOfOutput.Add(lines[indexOfLine]);
+        }
+
+        if (!File.Exists("fileOutput.txt"))
+        {
+            File.Create("fileOutput.txt");
+        }
+        var fileOutput = "fileOutput.txt";
+
+        File.WriteAllLines(fileOutput, arrayOfOutput);
+
+        var output = File.ReadAllLines(fileOutput);
+
+        foreach (var line in output)
+        {
+            Console.WriteLine(line);
+        }
+    }
+}
