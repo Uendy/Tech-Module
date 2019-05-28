9241311e34d928cda25c0a958edce87fd3fdbac5
Uendy <alex.beshkov98@gmail.com>
2019-05-28 19:20:31 +0300

    Adding Q02 Line Numbers.csproj

---
+++ L08 Files, Exceptions, Directories/L08 Lab V2/L08 Lab V2/Q02 Line Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Write a program that reads a text file and inserts line numbers in front of each of its lines.
+        //The result should be written to another text file. 
+
+        var file = "inputText.txt";
+
+        var lines = File.ReadAllLines(file);
+
+        //File.WriteAllText("resultText.txt", string.Empty); //to clear it if already exists
+        //var outPut = "resultText.txt";
+
+        var numberedLines = new List<string>();
+
+        for (int index = 0; index < lines.Count(); index++)
+        {
+            numberedLines.Add($"{index + 1}. {lines[index]}");
+        }
+
+        File.WriteAllLines("resultFile.txt", numberedLines);
+    }
+}
