1032052b2ed70f373f368ab788f5a3970db066fd
Uendy <alex.beshkov98@gmail.com>
2019-05-30 22:27:32 +0300

    Adding Q04 Merge Files.csproj

---
+++ L08 Files, Exceptions, Directories/L08 Lab V2/L08 Lab V2/Q04 Merge Files/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.IO;
+public class Program
+{
+    public static void Main()
+    {
+        //Write a program that reads the contents of two text files and merges them together into a third one.
+
+        var firstFile = "inputText1.txt";
+        var secondFile = "inputText2.txt";
+
+        var firstFileContents = File.ReadAllLines(firstFile);
+        var secondFileContents = File.ReadAllLines(secondFile);
+
+        var output = new List<string>();
+
+        for (int index = 0; index < firstFileContents.Length; index++)
+        {
+            output.Add(firstFileContents[index]);
+            output.Add(secondFileContents[index]);
+        }
+
+        File.WriteAllLines("outputText.txt", output);
+    }
+}
