c4a86a6c7c4532b5f27aa180ee4506a0e35cdd38
Uendy <alex.beshkov98@gmail.com>
2019-05-31 21:11:34 +0300

    Adding Q05 Folder Size.csproj

---
+++ L08 Files, Exceptions, Directories/L08 Lab V2/L08 Lab V2/Q05 Folder Size/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+public class Program
+{
+    public static void Main()
+    {
+        //You are given a folder named “TestFolder”. Get the size of all files in the folder, which are NOT directories. 
+        //The result should be written to another text file in Megabytes.
+
+        var currentFile = Directory.GetFiles("TestFolder");
+
+        double sum = 0.0;
+
+        foreach (var file in currentFile)
+        {
+            var fileInfo = new FileInfo(file);
+            sum += fileInfo.Length;
+        }
+
+        Console.WriteLine(sum / 1024.0 / 1024.0);
+    }
+}
