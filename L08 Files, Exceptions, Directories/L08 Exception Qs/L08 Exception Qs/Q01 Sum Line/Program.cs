706b8cc7a8a7d79b76c65be6863492d21dcca024
Uendy <alex.beshkov98@gmail.com>
2019-06-02 18:26:47 +0300

    Adding Q01 Sum Line.csproj

--- L08 Files, Exceptions, Directories/L08 Exception Qs/L08 Exception Qs/Q01 Sum Line/Program.cs
+++ L08 Files, Exceptions, Directories/L08 Exception Qs/L08 Exception Qs/Q01 Sum Line/Program.cs
@@ -2,14 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-
-namespace Q01_Sum_Line
+using System.IO;
+public class Program
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        //Write a program that reads a text file (input.txt from the Resources - Exercises)
+        //prints on the console the sum of the ASCII symbols of each of its lines.
+        //Use BufferedReader in combination with FileReader.
+
+        var file = "input.txt";
+
+        var linesInFile = File.ReadAllLines(file);
+
+        foreach (var line in linesInFile)
         {
+            int sum = line.Sum(b => b);
+            Console.WriteLine(sum);
         }
     }
 }
