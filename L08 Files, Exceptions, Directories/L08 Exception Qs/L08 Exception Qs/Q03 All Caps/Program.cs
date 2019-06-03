36493383c0faaf84e4c0f7a7c2b3f11575036d59
Uendy <alex.beshkov98@gmail.com>
2019-06-03 19:40:44 +0300

    Adding Q03 All Caps.csproj

---
+++ L08 Files, Exceptions, Directories/L08 Exception Qs/L08 Exception Qs/Q03 All Caps/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+public class Program
+{
+    public static void Main()
+    {
+        //Write a program that reads a text file (input.txt from the Resources - Exercises)  
+        //changes the casing of all letters to upper. 
+        //Write the output to another file (output.txt). 
+
+        string file = "inputText.txt";
+
+        if (File.Exists(file))
+        {
+            var textInFile = File.ReadAllText(file).ToUpper();
+
+            File.WriteAllText("outputText.txt", textInFile);
+        }
+        else
+        {
+            Console.WriteLine($"File {file} does not exist");
+            Environment.Exit(0);
+        }
+    }
+}
