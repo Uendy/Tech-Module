92665d475095d31bb6961a0b4164658d54fd1491
Uendy <alex.beshkov98@gmail.com>
2019-06-03 19:15:01 +0300

    Adding Q02 Sum Byte.csproj

---
+++ L08 Files, Exceptions, Directories/L08 Exception Qs/L08 Exception Qs/Q02 Sum Byte/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+public class Program
+{
+    public static void Main()
+    {
+        //Write a program that reads a text file (input.txt from the Resources - Exercises) 
+        //prints on the console the sum of the ASCII symbols of all characters inside of the file.
+        //Use BufferedReader in combination with FileReader.
+
+        string inputName = "inputText.txt";
+
+        var inputLines = new string[0];
+
+        if (File.Exists(inputName))
+        {
+             inputLines = File.ReadAllLines(inputName);
+        }
+        else
+        {
+            Console.WriteLine($"File {inputName} does not exist");
+            Environment.Exit(0);
+        }
+
+        long sum = 0;
+
+        foreach (var line in inputLines)
+        {
+            foreach (var charecter in line.ToCharArray())
+            {
+                sum += charecter;
+            }
+        }
+
+        Console.WriteLine(sum);
+    }
+}
