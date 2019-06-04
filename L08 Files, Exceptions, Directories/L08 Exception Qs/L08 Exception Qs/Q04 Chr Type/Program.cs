26320bb19c608d7e3d25d884c2b399b91eac3819
Uendy <alex.beshkov98@gmail.com>
2019-06-04 14:25:29 +0300

    Adding Q04 Chr Type.csproj

---
+++ L08 Files, Exceptions, Directories/L08 Exception Qs/L08 Exception Qs/Q04 Chr Type/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+using System.IO;
+public class Program
+{
+    public static void Main()
+    {
+        //Write a program that reads a list of words from the file (input.txt from the Resources - Exercises)  
+        //finds the count of vowels, consonants and punctuation marks. Assume that:
+        //•	a, e, i, o, u are vowels, only lower case
+        //•	All others are consonants
+        //•	Punctuation marks are(!,.?)
+        //•	Do not count whitespace.
+        //Write the results to another file – output.txt.
+
+        var inputName = "inputText.txt";
+
+        var inputFile = File.ReadAllLines(inputName);
+
+        int vowelCount = 0;
+        int consonantCount = 0;
+        int punctuationCount = 0;
+
+        foreach (var line in inputFile)
+        {
+            var lineAsArray = line.Replace(" ", "").ToCharArray();
+            foreach (var charecter in lineAsArray)
+            {
+                var vowelArray = new char[] { 'a', 'e', 'i', 'o', 'u' }; //only lowerCase
+                var punctuationArray = new char[] { '!', ',', ',', '?' };
+
+                bool isVowel = vowelArray.Contains(charecter);
+                if (isVowel)
+                {
+                    vowelCount++;
+                }
+                else
+                {
+                    bool isPunctuation = punctuationArray.Contains(charecter);
+                    if (isPunctuation)
+                    {
+                        punctuationCount++;
+                    }
+                    else // is a consonant
+                    {
+                        consonantCount++;
+                    }
+                }
+            }
+
+        }
+
+        var outPut = new string[3];
+        outPut[0] = $"Vowels: {vowelCount}";
+        outPut[1] = $"Consonants: {consonantCount}";
+        outPut[2] = $"Punctuation: {punctuationCount}";
+
+        File.WriteAllLines("output.txt", outPut);
+    }
+}
