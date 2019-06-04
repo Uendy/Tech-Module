a976b81d48b48efec68df9da32068064fe56a6f5
Uendy <alex.beshkov98@gmail.com>
2019-06-04 14:31:38 +0300

    Adding Q05 Line Num.csproj

---
+++ L08 Files, Exceptions, Directories/L08 Exception Qs/L08 Exception Qs/Q05 Line Num/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.IO;
+public class Program
+{
+    public static void Main()
+    {
+        //Write a program that reads a text file (inputLineNumbers.txt from the Resources - Exercises) 
+        //inserts line numbers in front of each of its lines.
+        //The result should be written to another text file – output.txt. 
+
+        var inputName = "inputText.txt";
+
+        var inputFile = File.ReadAllLines(inputName);
+
+        for (int index = 0; index < inputFile.Count(); index++)
+        {
+            inputFile[index] = $"{index + 1}. {inputFile[index]}";
+        }
+
+        File.WriteAllLines("output.txt", inputFile);
+    }
+}
