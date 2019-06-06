fcdc45b350db2d9698c4f9e55ed539b290dc5f53
Uendy <alex.beshkov98@gmail.com>
2019-06-06 20:04:18 +0300

    Adding Q07 Merger.csproj

---
+++ L08 Files, Exceptions, Directories/L08 Exception Qs/L08 Exception Qs/Q07 Merger/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+public class Program
+{
+    public static void Main()
+    {
+        //Write a program that reads the contents of two text files (inputOne.txt, inputTwo.txt from Resources Exercises) 
+        //merges them together into a third one.
+
+        var firstFile = "inputFileOne.txt";
+        var secondFile = "inputFileTwo.txt";
+
+        var firstFileContents = File.ReadAllLines(firstFile);
+        var secondFileContents = File.ReadAllLines(secondFile);
+
+        int maxInt = Math.Max(firstFileContents.Count(), secondFileContents.Count());
+
+        string outputFileName = "outputFile.txt";
+        File.WriteAllText(outputFileName, string.Empty);
+
+        for (int index = 0; index < maxInt; index++)
+        {
+            try
+            {
+                File.AppendAllText(outputFileName, firstFileContents[index] + Environment.NewLine);
+                File.AppendAllText(outputFileName, secondFileContents[index] + Environment.NewLine);
+            }
+            catch (Exception)
+            {
+
+                throw new Exception ("outside of array");
+            }
+        }
+    }
+}
