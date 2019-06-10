fc5ea9552d271c12d056b67520a81afd0b7ccc4e
Uendy <alex.beshkov98@gmail.com>
2019-06-10 21:40:54 +0300

    Adding Q08 Size.csproj

---
+++ L08 Files, Exceptions, Directories/L08 Exception Qs/L08 Exception Qs/Q08 Size/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Write a program that traverses a folder and calculates its size in bytes. 
+        //Use Folder Exercises Resources in Resources.
+
+        var totalSize = Directory.GetFiles("dir").Select(f => new FileInfo(f)).Sum(f => f.Length);
+        Console.WriteLine(totalSize);
+
+        if (Directory.Exists("dir"))
+        {
+            var files = Directory.GetFiles("dir");
+        }
+        //var rootDir = "dir";
+
+        ////var dirInfo = new DirectoryInfo(rootDir);
+
+        //var filesInDir = Directory.GetFiles(rootDir);
+
+        //long sumOfSize = 0;
+        //foreach (var file in filesInDir)
+        //{
+        //    sumOfSize += file.Length;
+        //}
+
+
+        //Console.WriteLine(sumOfSize);
+    }
+}
