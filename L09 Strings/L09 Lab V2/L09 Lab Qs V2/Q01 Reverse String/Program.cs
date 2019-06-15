41d3d85d8b391f4df727d6b50c11f5eb0642031a
Uendy <alex.beshkov98@gmail.com>
2019-06-15 17:17:15 +0300

    Adding Q01 Reverse String.csproj

---
+++ L09 Strings/L09 Lab V2/L09 Lab Qs V2/Q01 Reverse String/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    //Check any questions here: https://judge.softuni.bg/Contests/Practice/Index/320#0
+    public static void Main()
+    {
+        //Write a program that reads a string from the console, reverses it and prints the result back at the console.
+
+        string input = Console.ReadLine();
+        input = new string(input.ToCharArray().Reverse().ToArray());
+        Console.WriteLine(input);
+
+    }
+}
