f2bc41a925fc8a8de9bdb45ee54ded1c8729df47
Uendy <alex.beshkov98@gmail.com>
2019-06-17 20:59:23 +0300

    Adding Q03 Text Filter.csproj

---
+++ L09 Strings/L09 Lab V2/L09 Lab Qs V2/Q03 Text Filter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //Write a program that takes a text and a string of banned words.
+        //All words included in the ban list should be replaced with asterisks "*", equal to the word's length.
+        //The entries in the ban list will be separated by a comma and space ", ".
+        //The ban list should be entered on the first input line and the text on the second input line.
+
+        var bannedWords = Console.ReadLine();
+        var listOfBannedWords = bannedWords.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+        var dictOfBannedWords = new Dictionary<string, int>(); //key = word //value = .length
+        foreach (var word in listOfBannedWords)
+        {
+            dictOfBannedWords[word] = word.Length;
+        }
+
+        var text = Console.ReadLine();
+
+        foreach (var word in dictOfBannedWords.Keys)
+        {
+            var censor = new string('*', dictOfBannedWords[word]);
+            text = text.Replace(word, censor.ToString());
+        }
+
+        Console.WriteLine(text);
+    }
+}
