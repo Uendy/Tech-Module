fb43e844e2c5929b3f5c6fad28e584accd93cd2e
Uendy <alex.beshkov98@gmail.com>
2019-06-18 16:39:36 +0300

    Adding Q01 V2.csproj

---
+++ L09 Strings/L09 Lab V2/L09 Lab Qs V2/Q01 V2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //Write a program that reads a string from the console, reverses it and prints the result back at the console.
+
+        string input = Console.ReadLine();
+        string output = new string(input.ToCharArray().Reverse().ToArray());
+
+        Console.WriteLine(output);
+    }
+}
