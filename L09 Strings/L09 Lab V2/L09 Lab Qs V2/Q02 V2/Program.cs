689ebd084d252c4017638c87c4a61fb56ffcd1c0
Uendy <alex.beshkov98@gmail.com>
2019-06-18 16:52:55 +0300

    Adding Q02 V2.csproj

---
+++ L09 Strings/L09 Lab V2/L09 Lab Qs V2/Q02 V2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        //Write a program to find how many times a given string appears in a given text as substring. 
+        //The text is given at the first input line. 
+        //The search string is given at the second input line. 
+        //The output is an integer number. 
+        //Please ignore the character casing. 
+        //Overlapping between occurrences is allowed. 
+
+        string text = Console.ReadLine();
+        string substring = Console.ReadLine();
+        int occurances = 0;
+
+        int currentIndex = 0;
+
+        while (currentIndex <= text.Length - 1)
+        {
+            currentIndex = text.IndexOf(substring, currentIndex);
+            if (currentIndex < 0)
+            {
+                Console.WriteLine(occurances);
+                Environment.Exit(0);
+            }
+            else
+            {
+                occurances++;
+                currentIndex++;
+            }
+        }
+        Console.WriteLine(occurances);
+
+    }
+}
