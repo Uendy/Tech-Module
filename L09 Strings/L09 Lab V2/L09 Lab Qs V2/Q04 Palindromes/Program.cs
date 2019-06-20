56ea4737f4405889fa13a1b854cd80507b383d4f
Uendy <alex.beshkov98@gmail.com>
2019-06-20 20:49:18 +0300

    Adding Q04 Palindromes.csproj

---
+++ L09 Strings/L09 Lab V2/L09 Lab Qs V2/Q04 Palindromes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //Write a program that extracts from a given text all palindromes, e.g. ABBA, lamal, exe 
+        //prints them on the console on a single line, separated by comma and space. 
+        //Use spaces, commas, dots, question marks and exclamation marks as word delimiters.
+        //Print only unique palindromes, sorted lexicographically.
+
+        string inputText = Console.ReadLine();
+        var listOfStrings = inputText.Split(new[] { ',', '.', ' ', '?', '!' }, StringSplitOptions.RemoveEmptyEntries).Distinct().ToList();
+
+        var listOfPalindromes = new List<string>();
+        foreach (var word in listOfStrings)
+        {
+            int size = word.Length;
+            int halfSize = size / 2;
+
+            var wordAsCharArray = word.ToCharArray();
+
+            var firstHalfAsArray = wordAsCharArray.Take(halfSize).ToArray();
+            var firstHalf = string.Concat(firstHalfAsArray);
+            var secondHalfAsArray = wordAsCharArray.Reverse().Take(halfSize).ToArray();
+            var secondHalf = string.Concat(secondHalfAsArray);
+
+            if (firstHalf.Equals(secondHalf))
+            {
+                listOfPalindromes.Add(word);
+            }
+        }
+
+        string outPut = string.Join(", ", listOfPalindromes.OrderBy(x => x));
+        Console.WriteLine(outPut);
+    }
+}
