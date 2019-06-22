da080d402921cc53023e32cff31b477ac8e97495
Uendy <alex.beshkov98@gmail.com>
2019-06-22 20:20:29 +0300

    Adding Q03 Unicode Char.csproj

---
+++ L09 Strings/L09 Exercise V2/L09 Ex V2/Q03 Unicode Char/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //Create a method that takes two strings as arguments
+        ///return the sum of their character codes multiplied (multiply str1.charAt (0) with str2.charAt (0) and add to the total sum). 
+        //Then continue with the next two characters.
+        //If one of the strings is longer than the other, add the remaining character codes to the total sum without multiplication.
+
+        string input = Console.ReadLine();
+
+        var chars = input
+             .Select(c => (int)c)
+             .Select(c => $@"\u{c:x4}");
+
+        var result = string.Concat(chars);
+
+        Console.WriteLine(result);
+    }
+}
