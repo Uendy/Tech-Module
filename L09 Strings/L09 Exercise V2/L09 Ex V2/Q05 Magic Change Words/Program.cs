8d615f80bbd1d8c1f52218ccdda159e577ff4479
Uendy <alex.beshkov98@gmail.com>
2019-06-24 20:17:43 +0300

    Adding Q05 Magic Change Words.csproj

---
+++ L09 Strings/L09 Exercise V2/L09 Ex V2/Q05 Magic Change Words/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //Write a method that takes as input two strings, and returns Boolean if they are exchangeable or not.
+        //Exchangeable are words where the characters in the first string can be replaced to get the second string.
+        //Example: "egg" and "add" are exchangeable, but "aabbccbb" and "nnooppzz" are not.
+        //(First 'b' corresponds to 'o', but then it also corresponds to 'z').
+        //The two words may not have the same length, if such is the case they are 
+        //exchangeable only if the longer one doesn't have more types of characters then the shorter one 
+        //("Clint" and "Eastwaat" are exchangeable because 'a' and 't' are already mapped as 'l' and 'n', but "Clint" 
+        //"Eastwood" aren't exchangeable because 'o' and 'd' are not contained in "Clint").
+
+        var input = Console.ReadLine().Split(' ').ToArray();
+        var firstString = new string(input[0].ToCharArray().Distinct().ToArray());
+        var secondString = new string(input[1].ToCharArray().Distinct().ToArray());
+
+        if (firstString.Length != secondString.Length) 
+        {
+            Console.WriteLine("false");
+        }
+        else // Magic Number
+        {
+            Console.WriteLine("true");
+        }
+
+    }
+}
