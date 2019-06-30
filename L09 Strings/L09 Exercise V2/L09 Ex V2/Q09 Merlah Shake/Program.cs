c2b613c7c3c381cce5842b49b6d6bc2cda8123ab
Uendy <alex.beshkov98@gmail.com>
2019-06-30 13:55:22 +0300

    Adding outline of Q09 Merlah Shake.csproj

---
+++ L09 Strings/L09 Exercise V2/L09 Ex V2/Q09 Merlah Shake/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+public class Program
+{
+    public static void Main()
+    {
+        //You are given a string of random characters, and a pattern of random characters. 
+        //You need to “shake off” (remove) all of the border occurrences of that pattern, in other words:
+        // the very first match and the very last match of the pattern you find in the string.
+        //When you successfully shake off a match,
+        //you remove from the pattern the character which corresponds to the index equal to the pattern’s length / 2.
+        //Then you continue to shake off the border occurrences of the new pattern until,
+        //the pattern becomes empty or until there is less than the - needed for shake, matches in the remaining string.
+        //In case you have found at least two matches, and you have successfully shaken them off, 
+        //you print “Shaked it.” on the console.Otherwise you print “No shake.”, 
+        //the remains of the main string, and you end the program.See the examples for more info.
+
+    }
+}
