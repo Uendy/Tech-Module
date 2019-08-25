f0646d4dc58d69bad1b5093212917e518e3f5015
Uendy <alex.beshkov98@gmail.com>
2019-08-25 18:14:47 +0300

    Adding: -Folder L11 Test 05.11.17 Qs -Q01 Anonymous Downsite/Program.cs and Words document of Q -Q02 Anonymous Threat/Program.cs and Word document of Q
    Q01 Solution contains the link to check the questions

---
+++ L11 Test/Test 05.11.17/Test 05.11.17 Qs/Anonymous Threat/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+public class Program
+{
+    public static void Main()
+    {
+        #region
+        //The Anonymous have created a cyber hypervirus which steals data from the CIA. 
+        //You, as the lead security developer in CIA, 
+        //have been tasked to analyze the software of the virus and observe its actions on the data.
+        //The virus is known for his innovative and unbeleivably clever technique of merging and dividing data into partitions. 
+
+        //You will receive a single input line containing STRINGS separated by spaces. 
+        //The strings may contain any ASCII character except whitespace.
+        //You will then begin receiving commands in one of the following formats:
+
+        //•	merge { startIndex} { endIndex}
+        //•	divide { index} { partitions}
+
+        //Every time you receive the merge command, you must merge all elements from the startIndex, till the endIndex. 
+        //In other words, you should concatenate them. 
+        //Example: { abc, def, ghi} -> merge 0 1-> { abcdef, ghi}
+        //If any of the given indexes is out of the array, you must take ONLY the range that is INSIDE the array and merge it.
+        //Every time you receive the divide command, you must DIVIDE the element at the given index,
+        //into several small substrings with equal length.The count of the substrings should be equal to the given partitions. 
+        //Example: { abcdef, ghi, jkl} -> divide 0 3-> { ab, cd, ef, ghi, jkl}
+        //If the string CANNOT be exactly divided into the given partitions, make all partitions except the LAST with EQUAL LENGTHS,
+        //and make the LAST one – the LONGEST. 
+        //Example: { abcd, efgh, ijkl} -> divide 0 3-> { a, b, cd, efgh, ijkl}
+
+        //The input ends when you receive the command “3:1”. At that point you must print the resulting elements, joined by a space.
+
+        //Input
+        //•	The first input line will contain the array of data.
+        //•	On the next several input lines you will receive commands in the format specified above.
+        //•	The input ends when you receive the command “3:1”.
+
+        //Output
+        //•	As output you must print a single line containing the elements of the array, joined by a space.
+
+        //Constrains
+        //•	The strings in the array may contain any ASCII character except whitespace.
+        //•	The startIndex and the endIndex will be in range[-1000, 1000].
+        //•	The endIndex will ALWAYS be GREATER than the startIndex.
+        //•	The index in the divide command will ALWAYS be INSIDE the array.
+        //•	The partitions will be in range[0, 100].
+        //•	Allowed working time / memory: 100ms / 16MB.
+       #endregion
+
+        string input = Console.ReadLine();
+
+        var inputTokens = input.Split(' ').ToArray();
+        var tokens = new List<string>(inputTokens);
+
+        string command = Console.ReadLine();
+        while (command != "3:1")
+        {
+            var commandTokens = command.Split(' ').ToArray();
+            string acutalCommand = commandTokens[0];
+
+            if (acutalCommand == "merge")
+            {
+                int startIndex = int.Parse(commandTokens[1]);
+                int endIndex = int.Parse(commandTokens[2]);
+
+                //checking if end index is above cut off and if so bringing it down to the max
+                bool afterFinalIndex = endIndex > tokens.Count();
+                if (afterFinalIndex)
+                {
+                    endIndex = tokens.Count() - 1;
+                }
+
+                // putting all substrings in a massive string then removing all substring from tokens to return it as a single value
+                var sb = new StringBuilder();
+                for (int index = startIndex; index <= endIndex; index++)
+                {
+                    string currentString = tokens[index];
+                    sb.Append(currentString);
+                }
+
+                int lengthBetweenIndexs = endIndex - startIndex + 1; 
+
+                tokens.RemoveRange(startIndex, lengthBetweenIndexs);
+                tokens.Insert(startIndex, sb.ToString());
+            }
+            else // divide method
+            {
+                int index = int.Parse(commandTokens[1]);
+                int partitions = int.Parse(commandTokens[2]);
+
+                string currentString = tokens[index];
+                var asCharArray = currentString.ToCharArray();
+                int arraySize = asCharArray.Count();
+
+                var listOfSubStrings = new List<string>();
+
+                int partitionSize = arraySize / partitions;
+
+                var currentSubstring = string.Empty;
+
+                for (int innerIndex = 0; innerIndex < arraySize; innerIndex += partitionSize)
+                {
+                    bool finalIndex = innerIndex + partitions > arraySize;
+                    if (finalIndex)
+                    {
+                        bool evenSplit = arraySize % partitions == 0;
+                        if (evenSplit)
+                        {
+                            currentSubstring = currentString.Substring(innerIndex, partitionSize);
+                        }
+                        else // not an even split so last part has itself + remainder
+                        {
+                            var remainder = arraySize % partitions;
+                            currentSubstring = currentString.Substring(innerIndex, partitionSize + remainder);
+                            innerIndex += remainder;
+                        }
+                    }
+                    else
+                    {
+                        currentSubstring = currentString.Substring(innerIndex, partitionSize);
+                    }
+                    listOfSubStrings.Add(currentSubstring);
+                    currentSubstring = string.Empty;
+                } 
+
+                tokens.RemoveAt(index);
+                tokens.InsertRange(index, listOfSubStrings);
+            }
+
+            command = Console.ReadLine();
+        }
+
+        string output = string.Join(" ", tokens);
+        Console.WriteLine(output);
+    }
+}
---
+++ L11 Test/Test 05.11.17/Test 05.11.17 Qs/Q01 Anonymous Downsite/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+public class Program
+{
+    public static void Main()
+    {
+        //link to test and to check: https://judge.softuni.bg/Contests/Practice/Index/834#0
+
+        #region
+        //        The Anonymous informal group of activists have hacked a few commercial websites and the CIA has hired you to write a software which calculates the losses. Based on the given data, use the appropiate data types.
+        //You will receive 2 input lines – each containing an integer. 
+        //•	The first is N – the number of websites which are down.
+        //•	The second is the security key.
+        //On the next N lines you will receive data about websites in the following format:
+        //        { siteName}
+        //        { siteVisits}
+        //        { siteCommercialPricePerVisit}
+        //        You must calculate the site loss by the following formula: siteVisits* siteCommercialPricePerVisit
+        //When you finish reading all data, you must print the affected sites’ names – each on a new line.
+        //Then you must print the total money loss – sum of all site loss, on a new line.
+        //Finally you must print the security token, which is the security key, POWERED by the COUNT of affected sites.
+        //Input
+        //•	On the first input line you will get N – the count of affected websites.
+        //•	On the second input line you will the security key.
+        //•	On the next N input lines you will get data about the websites.
+        //Output
+        //•	As output you must print all affected websites’ names – each on a new line.
+        //•	After the website names you must print the total loss of data, printed to the 20th digit after the decimal point. The format is “Total Loss: { totalLoss}”.
+        //•	Finally you must print the security token.The format is “Security Token: { securityToken}”.
+        #endregion
+
+        int numberOfSites = int.Parse(Console.ReadLine());
+        int securityKey = int.Parse(Console.ReadLine());
+
+        var listOfSites = new List<string>();
+        decimal totalLoss = 0;
+
+        for (int i = 0; i < numberOfSites; i++)
+        {
+            var currentInput = Console.ReadLine();
+            var inputTokens = currentInput.Split(' ').ToArray();
+
+            string website = inputTokens[0];
+
+            listOfSites.Add(website);
+
+            long siteVisits = long.Parse(inputTokens[1]);
+            decimal payPerView = decimal.Parse(inputTokens[2]);
+
+            decimal currentLoss = siteVisits * payPerView;
+
+            totalLoss += currentLoss;
+        }
+
+        foreach (var website in listOfSites)
+        {
+            Console.WriteLine(website);
+        }
+        Console.WriteLine($"Total Loss: {totalLoss:F20}");
+        Console.WriteLine($"Security Token: {BigInteger.Pow(new BigInteger(securityKey), numberOfSites)}");
+    }
+}
