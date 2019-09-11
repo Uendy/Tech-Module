f40f7954033869a6efb72e995b5c183bd098b2fd
Uendy <alex.beshkov98@gmail.com>
2019-09-11 17:40:50 +0300

    Adding: -Exchange Method to Q02 Array Manipulator.csproj

--- L11 Test/Test Preparation IV/PT IV/Q02 Array Manipulator/Program.cs
+++ L11 Test/Test Preparation IV/PT IV/Q02 Array Manipulator/Program.cs
@@ -1,15 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Q02_Array_Manipulator
+public class Program
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        #region
+        //Trifon has finally become a junior developer and has received his first task. It’s about manipulating an array of integers.
+        //He is not quite happy about it, since he hates manipulating arrays.
+        //They are going to pay him a lot of money, though, and he is willing to give somebody half of it if to help him do his job. 
+        //You, on the other hand, love arrays(and money) so you decide to try your luck.
+
+        //The array may be manipulated by one of the following commands:
+
+        //•	exchange { index} – splits the array after the given index, and exchanges the places of the two resulting sub - arrays.
+        //E.g. [1, 2, 3, 4, 5]->exchange 2->result: [4, 5, 1, 2, 3]
+        //o If the index is outside the boundaries of the array, print “Invalid index”
+
+        //•	max even/odd– returns the INDEX of the max even/odd element -> [1, 4, 8, 2, 3] -> max odd -> print 4
+        //•	min even/odd – returns the INDEX of the min even/odd element -> [1, 4, 8, 2, 3] -> min even > print 3
+        //o If there are two or more equal min/max elements, return the index of the rightmost one
+        //o If a min/max even/odd element cannot be found, print “No matches”
+
+        //•	first {count} even/odd– returns the first {count} elements -> [1, 8, 2, 3] -> first 2 even -> print[8, 2]
+        //•	last {count} even/odd – returns the last {count} elements -> [1, 8, 2, 3] -> last 2 odd -> print[1, 3]
+        //o If the count is greater than the array length, print “Invalid count”
+        //o If there are not enough elements to satisfy the count, print as many as you can.If there are zero even/odd elements, print an empty array “[]”
+
+        //•	end – stop taking input and print the final state of the array
+        #endregion
+
+        var array = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+        string commandLine = Console.ReadLine();
+        while (commandLine != "end")
         {
+            var commandTokens = commandLine.Split(' ').ToList();
+
+            string command = commandTokens[0];
+            switch (command)
+            {
+                case "Exchange":
+                array = ExchangeMethod(array, commandTokens);
+                        break;
+                default:
+                    break;
+            }
+
+            commandLine = Console.ReadLine();
         }
+
+        string outPut = string.Join(" ", array);
+        Console.WriteLine(outPut);
+    }
+
+    public static List<int> ExchangeMethod(List<int> array, List<string> commandTokens)
+    {
+        int index = int.Parse(commandTokens[1]);
+
+        bool inside = index >= 0 && index < array.Count();
+        if (!inside)
+        {
+            Console.WriteLine("Invalid index");
+            return array;
+        }
+
+        var newEnd = array.GetRange(0, index);
+        array.RemoveRange(0, index);
+        array = array.Concat(newEnd).ToList();
+
+        return array;
     }
 }
