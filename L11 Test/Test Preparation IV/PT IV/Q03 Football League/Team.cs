4c340f690155e91cca5d56119893d92693697605
Uendy <alex.beshkov98@gmail.com>
2019-09-14 09:07:04 +0300

    Adding: -Q04 Cubic Messages.cs -Q04 Problem Description.docx

---
+++ L11 Test/Test Preparation IV/PT IV/Q03 Football League/Team.cs
@@ -0,0 +1,6 @@
+﻿public class Team
+{
+    public string Name { get; set; }
+    public int Goals { get; set; }
+    public int Points { get; set; }
+}
---
+++ L11 Test/Test Preparation IV/PT IV/Q04 Cubic Messages/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+    }
+}
