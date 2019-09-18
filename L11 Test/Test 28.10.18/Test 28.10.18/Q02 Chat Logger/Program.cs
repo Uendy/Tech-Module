e96f381a294f96f6d113bcc73fd70ec248ebdc7e
Uendy <alex.beshkov98@gmail.com>
2019-09-18 14:55:16 +0300

    Adding: -Q03 Chore Wars (70/100, hard to get under memory limit without Regex) -Q03 Chore Wars Problem Description

---
+++ L11 Test/Test 28.10.18/Test 28.10.18/Q02 Chat Logger/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //Your task is to implement chat logger which works with commands.
+        //You may receive the following commands:
+        //•	Chat { message} -add the message at last position in the chat
+        //•	Delete { messageToDelete}-delete the message if it exists
+        //•	Edit { messageToEdit}{ editedVersion} -update the message with the edited version
+        //•	Pin { message}-find the given message and move it to the last index
+        //•	Spam { message1}{ message2}{ messageN} -add all messages at the end of the chat
+        //•	end - stop receiving commands
+        //After the stop command, you should print the chat history starting from the first message.
+
+        var messages = new List<string>();
+
+        string input = Console.ReadLine();
+        while (input != "end")
+        {
+            var inputTokens = input.Split(' ').ToArray();
+
+            string command = inputTokens[0];
+
+            switch (command)
+            {
+                case "Chat":
+                    messages.Add(inputTokens[1]);
+                    break;
+
+                case "Delete":
+                    string message = inputTokens[1];
+                    messages = DeleteMessage(messages, message);
+                    break;
+
+                case "Edit":
+                    messages = EditMessages(messages, inputTokens);
+                    break;
+
+                case "Pin":
+                    string currentMessage = inputTokens[1];
+                    messages = PinMessage(messages, currentMessage);
+                    break;
+
+                case "Spam":
+                    messages = SpamMessages(messages, inputTokens);
+                    break;
+
+                default:
+                    break;
+            }
+
+            input = Console.ReadLine();
+        }
+
+        foreach (var message in messages)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public static List<string> SpamMessages(List<string> messages, string[] inputTokens)
+    {
+        var spam = inputTokens.Skip(1).ToList();
+        messages.AddRange(spam);
+
+        return messages;
+    }
+
+    public static List<string> PinMessage(List<string> messages, string currentMessage)
+    {
+        bool exists = messages.Contains(currentMessage);
+        if (exists)
+        {
+            messages.Remove(currentMessage);
+            messages.Add(currentMessage);
+        }
+
+        return messages;
+    }
+
+    public static List<string> EditMessages(List<string> messages, string[] inputTokens)
+    {
+        string oldMessage = inputTokens[1];
+        string newMessage = inputTokens[2];
+
+        bool exists = messages.Contains(oldMessage);
+        if (exists)
+        {
+            int indexOfOld = messages.IndexOf(oldMessage);
+            messages[indexOfOld] = newMessage;
+        }
+
+        return messages;
+    }
+
+    public static List<string> DeleteMessage(List<string> messages, string message)
+    {
+        bool exists = messages.Contains(message);
+        if (exists)
+        {
+            messages.Remove(message);
+        }
+
+        return messages;
+    }
+}
---
+++ L11 Test/Test 28.10.18/Test 28.10.18/Q03 Chore Wars/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //Your task is to help Pesho understand what his wife is telling him and calculate how much time it will take him to do all the chores. 
+        //You will receive commands until you receive the "wife is happy" line.
+
+        //Each valid command will give you information about a chore. You will have to calculate how much time each chore will take and how much time it will take him overall.
+        //The command is valid if it follows one of the following rules:
+        //•	A valid command for doing the dishes always starts with "<" and ends with ">" and has only lowercase letters and digits.
+        //•	A valid command for cleaning the house always starts with "[" and ends with "]" and has only uppercase letters and digits.
+        //•	A valid command for doing the laundry always starts with "{" and ends with "}" and has any character in the middle.
+        //All other commands are invalid, and you should just skip them.You have to sum the numbers inside each command and add them to the total time for the chore. 
+
+        //In the end you have to print the time it will take Pesho for the chores in the format:
+        //"Doing the dishes - {timeDishes} min."
+        //"Cleaning the house - {timeCleaning} min."
+        //"Doing the laundry - {timeLaundry} min."
+        //"Total - {totalMinutes} min."
+
+        int dishes = 0;
+        int cleaning = 0;
+        int laundy = 0;
+
+        var specialChars = new List<char[]>();
+        var dishesChars = new char[] { '<', '>' };
+        var cleaningChars = new char[] { '[', ']' };
+        var laundryChars = new char[] { '{', '}' };
+
+        specialChars.Add(dishesChars);
+        specialChars.Add(cleaningChars);
+        specialChars.Add(laundryChars);
+
+        string input = Console.ReadLine();
+        while (input != "wife is happy")
+        {
+            foreach (var charPair in specialChars)
+            {
+                var range = GetRange(input, charPair); 
+                bool wrongRange = range.Count() == 0;
+                if (wrongRange)
+                {
+                    continue;
+                }
+
+                switch (charPair[0])
+                {
+                    case '<':
+                        bool firstIsValid = FirsValidation(range);
+                        while (firstIsValid == false)
+                        {
+                            var rangeAsString = string.Join("", range);
+                            range = GetRange(rangeAsString + ">", charPair); // this is a hack, not a job well done
+                            firstIsValid = FirsValidation(range);
+                        }
+
+                        dishes += CalculateTime(range);
+                        break;
+
+                    case '[':
+                        bool secondIsValid = SecondValidation(range); // and has only uppercase letters and digits.
+                        cleaning += CalculateTime(range);
+                        break;
+
+                    case '{':
+                        // no validation needed
+                        laundy += CalculateTime(range);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+
+            input = Console.ReadLine();
+        }
+
+        Console.WriteLine($"Doing the dishes - {dishes} min.");
+        Console.WriteLine($"Cleaning the house - {cleaning} min.");
+        Console.WriteLine($"Doing the laundry - {laundy} min.");
+
+        int totalTime = dishes + cleaning + laundy;
+
+        Console.WriteLine($"Total - {totalTime} min.");
+    }
+
+    public static int CalculateTime(List<char> range)
+    {
+        int time = 0;
+
+        foreach (var character in range)
+        {
+            bool isDigit = char.IsDigit(character);
+            if (isDigit)
+            {
+                int currentTime = int.Parse(character.ToString());
+                time += currentTime;
+            }
+        }
+
+        return time;
+    }
+
+    public static bool SecondValidation(List<char> range)
+    {
+        bool isValid = true;
+
+        foreach (var character in range)
+        {
+            bool correct = char.IsDigit(character) || character >= 65 && character <= 90;
+            if (!correct)
+            {
+                isValid = false;
+            }
+        }
+
+        return isValid;
+    }
+
+    public static bool FirsValidation(List<char> range)
+    {
+        bool isValid = true;
+
+        foreach (var character in range)
+        {
+            bool correct = char.IsDigit(character) || character >= 97 && character <= 122;
+            if (!correct)
+            {
+                return false;
+            }
+        }
+
+        return isValid;
+    }
+
+    public static List<char> GetRange(string input, char[] charPair)
+    {
+        char start = charPair[0];
+        char end = charPair[1];
+
+        var indexOfStart = input.IndexOf(start);
+        var indexOfEnd = input.IndexOf(end);
+
+        if (indexOfStart == -1 || indexOfEnd == -1)
+        {
+            return new List<char>();
+        }
+
+        var inputAsArray = input.ToCharArray().ToList();
+
+        var lengthOfRange = indexOfEnd - indexOfStart;
+
+        var range = inputAsArray.GetRange(indexOfStart + 1, lengthOfRange - 1);
+        return range;
+    }
+}
