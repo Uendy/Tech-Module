ecb27a09a24fffb5390f8002d6a8e3edaa2f40dc
Uendy <alex.beshkov98@gmail.com>
2019-09-29 20:38:52 +0300

    Updating: -Q03 Emoji Sumator (First Regex Q)

--- L11 Test/Test 24.03.19/Test 24.03.19/Q03 Emoji Sumator/Program.cs
+++ L11 Test/Test 24.03.19/Test 24.03.19/Q03 Emoji Sumator/Program.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Text;
 public class Program
 {
     public static void Main()
     {
+        #region
         //Create a program, that finds all of the emojis in a given message and makes some calculations.
         //You will receive a few lines of input. On the first line, you will receive a single line of text (ASCII symbols).
-        
+
         //On the next line, you will receive an emoji code in the following format:
         //“number: number: number: (…)”
         //Each number is the value of an ASCII symbol and if you decrypt all of the symbols, you will receive a name of an emoji.
@@ -25,6 +27,62 @@ public class Program
         //Example:
         //Emojis found: :sunny:, :smiley:, :smiley:
         //Total Emoji Power: { sum}
+        #endregion
 
+        string text = Console.ReadLine();
+
+        //to find the special emoji
+        var emojiArray = Console.ReadLine().Split(new[] { ':' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+        var sb = new StringBuilder();
+        foreach (var character in emojiArray)
+        {
+            char currentChar = (char)character;
+            sb.Append(currentChar);
+        }
+
+        var specialEmoji = sb.ToString();
+
+        //get the regex right and find any matches
+        string pattern = @"\s:[a-z]{4,}:(\s|\,|\.|\!|\?)";
+        var regex = new Regex(pattern);
+        var matches = regex.Matches(text);
+
+        var listOfExtendedEmojis = new List<string>(); //containing any the symbols around the 4+ smaller letters
+        var listOfReducedEmojis = new List<string>(); // containing only the 4+ letters
+        var sum = 0;
+        var emojiPattern = @"[a-z]{4,}"; //[a-z]{4,}
+        var emojiRegex = new Regex(emojiPattern);
+
+        foreach (Match match in matches)
+        {
+            var matchAsString = match.ToString();
+            listOfExtendedEmojis.Add(matchAsString);
+
+            var emojiOnly = emojiRegex.Match(matchAsString); // removes anything but the letters we need to calculate sum
+            var emoji = emojiOnly.ToString();
+
+            listOfExtendedEmojis.Add(emoji);
+
+            int currentSum = 0; //getting the ASCII value of the emoji
+            var emojiCharArray = emoji.ToCharArray();
+            foreach (var character in emojiCharArray)
+            {
+                var asciiValue = (int)character;
+                currentSum += asciiValue;
+            }
+
+            sum += currentSum;
+        }
+
+        //printing 
+        string emojiOutput = string.Join(",", listOfExtendedEmojis);
+        Console.WriteLine($"Emojis found: {emojiOutput}");
+
+        bool isSpecial = listOfExtendedEmojis.Contains(specialEmoji); //If any of the valid emoji names is equal to the special emoji code and if it is – multiply the total emoji power by 2
+        if (isSpecial)
+        {
+            sum *= 2;
+        }
+        Console.WriteLine($"Total Emoji Power: {sum}");
     }
 }
