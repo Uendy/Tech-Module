4fe77cb3f0a98e511a09b1bd4293833dd2d542ad
Uendy <alex.beshkov98@gmail.com>
2019-10-01 22:23:12 +0300

    Final Update: -Q03 V2 Regex

--- L11 Test/Test 10.01.19/Test 10.01.19/Q03 V3 Regex/Program.cs
+++ L11 Test/Test 10.01.19/Test 10.01.19/Q03 V3 Regex/Program.cs
@@ -1,30 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text.RegularExpressions;
+using System.Text;
 public class Program
 {
     public static void Main()
     {
         int decoder = int.Parse(Console.ReadLine());
 
-        var names = new List<string>();
+        var kids = new List<string>();
 
-        string namePattern = @"@([A-z] +).*\!G\!"; // group 1 == the name
-        //@"@([a-z]|[A-Z])+";
-        //string goodListPattern = @"!G!";
+        string pattern = @"@([A-Za-z]+)([^-@!:>]+)?\!G\!";  // group 1 == the name
 
         string input = Console.ReadLine();
         while (input != "end")
         {
+            string decodedString = DecodeString(input, decoder);
+
+            bool isMatch = Regex.IsMatch(decodedString, pattern);
+            if (isMatch)
+            {
+                Match match = Regex.Match(decodedString, pattern);
+
+                string name = match.Groups[1].Value;
+
+                kids.Add(name);
+            }
 
             input = Console.ReadLine();
         }
 
-        foreach (var kid in names)
+        foreach (var kid in kids)
         {
             Console.WriteLine(kid);
         }
 
+    }
+
+    //return decoded string: decode by subtracting the key from the value of each character.
+    public static string DecodeString(string input, int decoder)
+    {
+        var inputAsArray = input.ToCharArray();
+        var sb = new StringBuilder();
+        foreach (var character in inputAsArray)
+        {
+            char newChar = (char)(character - decoder);
+            sb.Append(newChar);
+        }
+
+        string outPut = sb.ToString();
+        return outPut;
     }
 }
