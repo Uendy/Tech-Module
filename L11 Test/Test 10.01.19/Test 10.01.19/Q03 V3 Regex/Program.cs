a32090b76582926eacf83a74b84de12ed9ac244c
Uendy <alex.beshkov98@gmail.com>
2019-10-01 21:25:49 +0300

    Adding: Q03 V3 Regex(initial)

---
+++ L11 Test/Test 10.01.19/Test 10.01.19/Q03 V3 Regex/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+public class Program
+{
+    public static void Main()
+    {
+        int decoder = int.Parse(Console.ReadLine());
+
+        var names = new List<string>();
+
+        string namePattern = @"@([A-z] +).*\!G\!"; // group 1 == the name
+        //@"@([a-z]|[A-Z])+";
+        //string goodListPattern = @"!G!";
+
+        string input = Console.ReadLine();
+        while (input != "end")
+        {
+
+            input = Console.ReadLine();
+        }
+
+        foreach (var kid in names)
+        {
+            Console.WriteLine(kid);
+        }
+
+    }
+}
