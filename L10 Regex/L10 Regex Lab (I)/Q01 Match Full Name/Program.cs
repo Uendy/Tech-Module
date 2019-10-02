5b55e358c9ef84daaedf5b13ea50536a72954e88
Uendy <alex.beshkov98@gmail.com>
2019-10-02 11:53:10 +0300

    Adding: -Folder: L10 Regex Lab (I) -Folder: L10 Regex Lab (II)

---
+++ L10 Regex/L10 Regex Lab (I)/Q01 Match Full Name/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q01_Match_Full_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //check questions here: https://judge.softuni.bg/Contests/Practice/Index/442#0
+        }
+    }
+}
---
+++ L10 Regex/L10 Regex Lab (II)/Q01 Reverse String/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q01_Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Check questions here: https://judge.softuni.bg/Contests/Practice/Index/320#0
+        }
+    }
+}
