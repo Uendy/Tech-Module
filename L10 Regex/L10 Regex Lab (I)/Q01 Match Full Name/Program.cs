1611a4d9a76816b54b051f5401647694952381d8
Uendy <alex.beshkov98@gmail.com>
2019-10-03 16:40:15 +0300

    Final Update: - Q01 Match Full Name/Program.cs

--- L10 Regex/L10 Regex Lab (I)/Q01 Match Full Name/Program.cs
+++ L10 Regex/L10 Regex Lab (I)/Q01 Match Full Name/Program.cs
@@ -1,16 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Q01_Match_Full_Name
+using System.Text.RegularExpressions;
+public class Program
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        //check questions here: https://judge.softuni.bg/Contests/Practice/Index/442#0
+
+        //Write a regular expression to match a valid full name.
+        //A valid full name consists of two words, each word starts with a capital letter and contains only lowercase letters afterwards; 
+        //each word should be at least two letters long; the two words should be separated by a single space. 
+
+        string text = Console.ReadLine();
+
+        string pattern = @"\b[A-Z][a-z]{2,}\s[A-Z][a-z]{2,}";
+
+        var regex = new Regex(pattern);
+
+        var matches = regex.Matches(text);
+
+        foreach (Match match in matches)
         {
-            //check questions here: https://judge.softuni.bg/Contests/Practice/Index/442#0
+            Console.WriteLine(match.Value);
         }
     }
 }
