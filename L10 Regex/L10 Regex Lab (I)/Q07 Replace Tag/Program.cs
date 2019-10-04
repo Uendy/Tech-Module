083f79361b392d7f6904396e15dd52926d0eabc2
Uendy <alex.beshkov98@gmail.com>
2019-10-04 16:42:39 +0300

    Adding: -Q07 Replace Tag initial

---
+++ L10 Regex/L10 Regex Lab (I)/Q07 Replace Tag/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+public class Program
+{
+    public static string Main()
+    {
+        //Write a program that replaces in a HTML document given as string all the tags <a href=…>…</a> with corresponding tags [URL href=…>…[/URL].
+        //Read an input, until you receive “end” command. Print the result on the console. 
+
+        //ex input: <ul> <li> <a href="http://softuni.bg">SoftUni</a>    
+        //</ li > </ ul >
+        //end
+
+        //ex output: 
+        //<ul> <li>
+        //[URL href="http://softuni.bg">SoftUni[/URL]
+        // </li>
+        //</ul>
+        // </li> </ul>
+
+        var text = "< ul > < li > < a href = ""http://softuni.bg"" > SoftUni </ a > </ li > </ ul > end";
+
+
+    }
+}
