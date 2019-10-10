991af04a6af0b46d129511bce72f699189a3b054
Uendy <alex.beshkov98@gmail.com>
2019-10-10 21:55:07 +0300

    Adding: -Q02 V2

---
+++ L12 TM+/L05+ Bitwise Opertations/L05+ Lab/Q02 V2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        var input = int.Parse(Convert.ToString(int.Parse(Console.ReadLine()),2));
+        var shifted = input >> 1;
+        Console.WriteLine(shifted & 1);
+
+    }
+}
