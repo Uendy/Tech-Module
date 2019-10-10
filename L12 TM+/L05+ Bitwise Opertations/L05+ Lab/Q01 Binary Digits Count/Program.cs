b256c19d964d406bf15e7c9ffff706db9447f8ff
Uendy <alex.beshkov98@gmail.com>
2019-10-10 08:46:06 +0300

    Adding: -Q01 Binary Digits Count -Q01 V2

---
+++ L12 TM+/L05+ Bitwise Opertations/L05+ Lab/Q01 Binary Digits Count/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        //You are given a positive integer number and one binary digit B(0 or 1). 
+        //Your task is to write a program that finds the number of binary digits(B) in a number.
+        //Examples
+        //Input   Output     Comments
+        //20
+        //0         3       20-> 10100
+
+        //15
+        //1         4         15-> 1111
+
+        //10
+        //0         2         10-> 1010
+
+
+        int input = int.Parse(Console.ReadLine());
+        int bit = int.Parse(Console.ReadLine());
+
+        int total = 0;
+        if (bit == 0)
+        {
+            while (input > 1)
+            {
+                bool anotherZero = input % 2 == 0;
+                if (anotherZero)
+                {
+                    total++;
+                }
+                input /= 2;
+            }
+        }
+        else //bit == 1
+        {
+            while (input != 0)
+            {
+                bool anotherOne = input % 2 != 0;
+                if (anotherOne)
+                {
+                    total++;
+                }
+                input /= 2;
+            }
+        }
+
+        Console.WriteLine(total);
+    }
+}
---
+++ L12 TM+/L05+ Bitwise Opertations/L05+ Lab/Q01 V2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+public class Program
+{
+    public static void Main()
+    {
+        int input = int.Parse(Console.ReadLine());
+        string searchedBit = Console.ReadLine();
+
+        string binary = Convert.ToString(input, 2); //convert to binary
+
+        string pattern = $@"{searchedBit}";
+        var regex = new Regex(pattern);
+        var matches = regex.Matches(binary).Count;
+        Console.WriteLine(matches);
+    }
+}
