eff9d8202f56c6c830d06849dbafd4dce85f4337
Uendy <alex.beshkov98@gmail.com>
2019-10-10 21:29:23 +0300

    Adding: -Q02 First Bit

---
+++ L12 TM+/L05+ Bitwise Opertations/L05+ Lab/Q02 First Bit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //Write a program that prints the bit at position 1 of a number.
+        //Examples
+        //Input     Output
+        //   2         1
+        //   51        1
+        //   13        0
+        //   24        0
+
+        var input = Convert.ToString(int.Parse(Console.ReadLine()), 2);
+        var firstbit = input.Reverse().Skip(1).First(); // reverse it, skip the last (now first index) and take the bit at position number 1
+        Console.WriteLine(firstbit);
+    }
+}
