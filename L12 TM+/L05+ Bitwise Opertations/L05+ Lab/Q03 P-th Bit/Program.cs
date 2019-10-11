6c58eff3ea04f756e73b2a1a6d35bb629ebc172a
Uendy <alex.beshkov98@gmail.com>
2019-10-11 17:50:58 +0300

    Adding: -Q03 P-th Bit

---
+++ L12 TM+/L05+ Bitwise Opertations/L05+ Lab/Q03 P-th Bit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //Write a program that prints the bit at position p of a number.
+
+        //Examples
+        //Input   Output
+        // 2145 
+        //   5      1
+        
+        //  512
+        //   0      0
+          
+        //  111
+        //   8      0
+        
+        //  255
+        //   7      1
+
+    }
+}
