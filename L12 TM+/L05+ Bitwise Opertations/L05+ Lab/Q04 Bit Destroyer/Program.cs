1c58ef433e8d75e2db5f35ee9d1c25287a9f92f9
Uendy <alex.beshkov98@gmail.com>
2019-10-12 12:18:47 +0300

    Adding: -Q04 Bit Destroyer

---
+++ L12 TM+/L05+ Bitwise Opertations/L05+ Lab/Q04 Bit Destroyer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        //Write a program that sets the bit at position p to 0. Print the resulting number.
+        // Examples
+        //  Input   Output
+        //   1313     
+        //   5        1281
+
+        //   231      
+        //   2        227
+
+        //   111      
+        //   6        47
+
+        int number = int.Parse(Console.ReadLine());  
+        int position = int.Parse(Console.ReadLine()); 
+
+        number &= ~(1 << position);
+        Console.WriteLine(number);
+    }
+}
