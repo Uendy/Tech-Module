b37fe937a2725b962c150cf7a9a621fc1bd7171a
Uendy <alex.beshkov98@gmail.com>
2019-10-13 08:13:31 +0300

    Adding: -Q05 Odd Times

---
+++ L12 TM+/L05+ Bitwise Opertations/L05+ Lab/Q05 Odd Times/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //Given an array of positive integers in a single line joined by space(' '). 
+        //All numbers occur even number of times except one number which occurs odd number of times. Find the number.
+        // Examples
+        //     Input           Output
+        // 1 2 3 2 3 1 3         3
+        // 5 7 2 7 5 2 5         5
+
+        var numbers = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        int number = numbers[0];
+
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            number ^= numbers[i];
+        }
+
+        Console.WriteLine(number);
+    }
+}
