be37a65a656b935f7d741e94badb4f88ff231370
Uendy <alex.beshkov98@gmail.com>
2019-10-15 10:46:49 +0300

    Adding: -Q06 Tri-Bit Switch

---
+++ L12 TM+/L05+ Bitwise Opertations/L05+ Lab/Q06 Tri-Bit Switch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //Write a program that inverts the 3 bits from position p to the left with their opposites (e.g. 111 -> 000, 101 -> 010) in 32-bit number.
+        //Print the resulting number on the console.
+
+        //Examples
+
+        //Input       Output                     Details
+        //1234         1874           00000000000000000000010011010010
+        // 7                          00000000000000000000011101010010
+
+        //44444        44524          00000000000000001010110110011100
+        //4                           00000000000000001010110111101100
+
+        int num = int.Parse(Console.ReadLine());
+        int startingPosition = int.Parse(Console.ReadLine());
+
+        long mask = 7 << startingPosition;
+        long result = num ^ mask;
+
+        Console.WriteLine(result);
+    }
+}
