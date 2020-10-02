1eaab31ad7059d170173e3c576e6ddc9888970da
Uendy <alex.beshkov98@gmail.com>
2020-10-02 18:26:34 +0300

    Adding: -Q03 Practice Others
    Updated:
    -Q02 Practice Float (to make it uniform)

--- L02 Data Types and Variables/L02 Qs (V3)/L02 Qs/Q02 Practice Float/Program.cs
+++ L02 Data Types and Variables/L02 Qs (V3)/L02 Qs/Q02 Practice Float/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 public class Program
 {
-    static void Main()
+    public static void Main()
     {
         // Instructions:
         // Create a new C# project and create a program that assigns floating point values to variables. Be sure that each value is stored in the correct variable type (try to find the most suitable variable type in order to save memory). Finally, you need to print all variables to the console.
---
+++ L02 Data Types and Variables/L02 Qs (V3)/L02 Qs/Q03 Practice Others/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        // Instructions:
+        // Create a new C# project and create a program that assigns character and string values to variables. Be sure that each value is stored in the correct variable. Finally, you need to print all variables to the console.
+
+        // Ex: 
+        // Software University
+        // B
+        // y
+        // e
+        // I love programming
+
+        // Reading input:
+        string firstString = Console.ReadLine();
+        char firstChar = char.Parse(Console.ReadLine());
+        char secondChar = char.Parse(Console.ReadLine());
+        char thirdChar = char.Parse(Console.ReadLine());
+        string secondString = Console.ReadLine();
+
+        // Printing output: 
+        Console.WriteLine(firstString);
+        Console.WriteLine(firstChar);
+        Console.WriteLine(secondChar);
+        Console.WriteLine(thirdChar);
+        Console.WriteLine(secondString);
+    }
+}
