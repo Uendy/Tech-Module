11e71fe3215bbea84a18b1272cf3a9fe5f701fd0
Uendy <alex.beshkov98@gmail.com>
2020-10-02 18:15:54 +0300

    Adding: -Q02 Practice Float

---
+++ L02 Data Types and Variables/L02 Qs (V3)/L02 Qs/Q02 Practice Float/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+public class Program
+{
+    static void Main()
+    {
+        // Instructions:
+        // Create a new C# project and create a program that assigns floating point values to variables. Be sure that each value is stored in the correct variable type (try to find the most suitable variable type in order to save memory). Finally, you need to print all variables to the console.
+
+        // Ex input: 
+        // 3.141592653589793238
+        // 1.60217657
+        // 7.8184261974584555216535342341
+
+        // Reading decimal numbers
+        double doubleNum = double.Parse(Console.ReadLine());
+        float floatNum = float.Parse(Console.ReadLine());
+        decimal decimalNum = decimal.Parse(Console.ReadLine());
+
+
+
+        // Printing decimal numbers
+        Console.WriteLine(doubleNum);
+        Console.WriteLine(floatNum);
+        Console.WriteLine(decimalNum);
+    }
+}
