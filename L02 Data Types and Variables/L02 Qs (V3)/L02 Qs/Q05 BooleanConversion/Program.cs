cbe3c8c9b7ce74606f3af3dd527e5cb47794b789
Uendy <alex.beshkov98@gmail.com>
2020-10-03 17:59:39 +0300

    Adding: -Q05 Boolean Conversion

---
+++ L02 Data Types and Variables/L02 Qs (V3)/L02 Qs/Q05 BooleanConversion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        // Requirment: Write a program that reads a string, converts it to Boolean variable and prints “Yes” if the variable is true and “No” if the variable is false.
+
+        // Example:
+        //  Input	Output
+        //  True     Yes
+        //  False    No
+
+        // Reading input:
+        string input = Console.ReadLine();
+
+        // Check if input is boolean value and converting:
+        if (Boolean.TryParse(input, out bool converted))
+        {
+            // Printing output:
+            if (converted == true)
+            { 
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
