e8cd096ffd6f54f2b9d1484ffb71e9ca2d580dd1
Uendy <alex.beshkov98@gmail.com>
2020-10-03 17:50:34 +0300

    Adding: -Q04 Hexdec Conversion

---
+++ L02 Data Types and Variables/L02 Qs (V3)/L02 Qs/Q04 Hexdec Conversion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        // Requirment: Write a program that reads a number in hexadecimal format (0x##) convert it to decimal format and prints it.
+
+        // Example: 
+        // Input	Output
+        //  0xFE    254
+        //  0x37    55
+        //  0x10    16
+
+        // Reading input:
+        string input = Console.ReadLine();
+
+        // Converting:
+        int decimalNum = Convert.ToInt32(input, 16);
+
+        // Printing output:
+        Console.WriteLine(decimalNum);
+    }
+}
