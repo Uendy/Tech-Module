c323ac6b62f61fbe8122b2916eadc024d0fa6146
Uendy <alex.beshkov98@gmail.com>
2020-10-06 17:46:51 +0300

    Adding: -Q06 Strings and Objects

---
+++ L02 Data Types and Variables/L02 Qs (V3)/L02 Qs/Q06 String and Object/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        // Instructions: Declare two string variables and assign them with “Hello” and “World”. Declare an object variable and assign it with the concatenation of the first two variables (mind adding an interval between). Declare a third string variable and initialize it with the value of the object variable (you should perform type casting).
+
+        // Example: 
+        // Input      Output
+        // Hello    Hello World
+        // World
+
+        // Reading input:
+        string firstString = Console.ReadLine();
+        string secondString = Console.ReadLine();
+
+        // Concatinate both string:
+        string concat = string.Concat(firstString, " ", secondString);
+
+        // Printing ouput:
+        Console.WriteLine(concat);
+    }
+}
