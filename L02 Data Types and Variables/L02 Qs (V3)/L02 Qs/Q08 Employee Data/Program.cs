b37586573db21edb8624865e5fe6ca1bcd0610a5
Uendy <alex.beshkov98@gmail.com>
2020-10-07 20:50:11 +0300

    Update: -finished Q08

--- L02 Data Types and Variables/L02 Qs (V3)/L02 Qs/Q08 Employee Data/Program.cs
+++ L02 Data Types and Variables/L02 Qs (V3)/L02 Qs/Q08 Employee Data/Program.cs
@@ -1,27 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 public class Program
 {
     public static void Main()
     {
-    // Instructions: A marketing company wants to keep record of its employees. Each record would have the following characteristics:
-    // •	First name
-    // •	Last name
-    // •	Age(0...100)
-    // •	Gender(m or f)
-    // •	Personal ID number(e.g. 8306112507)
-    // •	Unique employee number(27560000…27569999)
-    // Declare the variables needed to keep the information for a single employee using appropriate primitive data types. Use descriptive names.Print the data at the console.
+        // Instructions: A marketing company wants to keep record of its employees. Each record would have the following characteristics:
+        // •	First name
+        // •	Last name
+        // •	Age(0...100)
+        // •	Gender(m or f)
+        // •	Personal ID number(e.g. 8306112507)
+        // •	Unique employee number(27560000…27569999)
+        // Declare the variables needed to keep the information for a single employee using appropriate primitive data types. Use descriptive names.Print the data at the console.
 
-    // Examples
-    //  Input                       Output
-    //  Amanda              First name: Amanda
-    //  Jonson              Last name: Jonson
-    //  27                  Age: 27
-    //  f                   Gender: f
-    //  8306112507          Personal ID: 8306112507
-    //  27563571            Unique Employee number: 27563571
+        // Examples
+        //  Input                       Output
+        //  Amanda              First name: Amanda
+        //  Jonson              Last name: Jonson
+        //  27                  Age: 27
+        //  f                   Gender: f
+        //  8306112507          Personal ID: 8306112507
+        //  27563571            Unique Employee number: 27563571
 
+        // Reading input:
+        string firstName = Console.ReadLine();
+        string lastName = Console.ReadLine();
+        byte age = byte.Parse(Console.ReadLine());
+        char gender = char.Parse(Console.ReadLine());
+        long id = long.Parse(Console.ReadLine());
+        int uniqueEmployeeNumber = int.Parse(Console.ReadLine());
+
+        // Printing output;
+        Console.WriteLine($"First name: {firstName}");
+        Console.WriteLine($"Last name: {lastName}");
+        Console.WriteLine($"Age: {age}");
+        Console.WriteLine($"Gender: {gender}");
+        Console.WriteLine($"Personal ID: {id}");
+        Console.WriteLine($"Unique Employee number: {uniqueEmployeeNumber}");
     }
 }
