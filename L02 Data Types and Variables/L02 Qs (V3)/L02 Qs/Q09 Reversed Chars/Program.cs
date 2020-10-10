7b3c066962ea72d47ea222c67c53e93b4bbb786f
Uendy <alex.beshkov98@gmail.com>
2020-10-10 14:23:44 +0300

    Adding: -Q09 Reversed Chars

---
+++ L02 Data Types and Variables/L02 Qs (V3)/L02 Qs/Q09 Reversed Chars/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        // Instructions: Write a program to ask the user for 3 letters and print them in reversed order.
+
+        // Example:
+        // Input     Output
+        //   A        CBA
+        //   B
+        //   C
+
+        // Reading input:
+        char firstChar = char.Parse(Console.ReadLine());
+        char secondChar = char.Parse(Console.ReadLine());
+        char thirdChar = char.Parse(Console.ReadLine());
+
+        // Printing output:
+        Console.WriteLine($"{thirdChar}{secondChar}{firstChar}");
+    }
+}
