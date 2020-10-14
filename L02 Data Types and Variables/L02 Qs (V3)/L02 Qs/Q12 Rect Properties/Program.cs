93fcc26225ae4cd4ef5a3f49d16e59570c0971d9
Uendy <alex.beshkov98@gmail.com>
2020-10-14 20:06:54 +0300

    Adding: -Q12 Rectangle Properties (Rect Properties)

---
+++ L02 Data Types and Variables/L02 Qs (V3)/L02 Qs/Q12 Rect Properties/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        // Instructions: Create a program to calculate rectangle’s perimeter, area and diagonal by given its width and height.
+
+        // Example:
+        //  Input:          Output:
+        //   10               30
+        //   5                50
+        //                  11.1803398874989
+
+        // Example 2:
+        //  Input:          Output:
+        //   22.1            64.6
+        //   10.2            225.42
+        //              24.3402958075698
+
+        // Reading input: (width and height of rectangle)
+        double width = double.Parse(Console.ReadLine());
+        double height = double.Parse(Console.ReadLine());
+
+        // Calculations:
+        double perimeter = width * 2 + height * 2;
+        double area = width * height;
+        double diagonal = Math.Sqrt(Math.Pow(width, 2) + Math.Pow(height, 2));
+
+        // Printing output:
+        Console.WriteLine(perimeter);
+        Console.WriteLine(area);
+        Console.WriteLine(diagonal);
+    }
+}
