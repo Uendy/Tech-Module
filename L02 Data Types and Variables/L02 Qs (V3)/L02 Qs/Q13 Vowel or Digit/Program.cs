50ffe33b119132e34f27d2cc02b3660c5fc133e3
Uendy <alex.beshkov98@gmail.com>
2020-10-15 09:01:14 +0300

    Adding: -Q13 Vowel or Digit

---
+++ L02 Data Types and Variables/L02 Qs (V3)/L02 Qs/Q13 Vowel or Digit/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+public class Program
+{
+    public static void Main()
+    {
+        // Instructions: Create a program to check if given symbol is digit, vowel or any other symbol.
+
+        // Example:
+        // Intro:       Outro:
+        //  a           vowel
+        //  9           digit
+        //  g           other
+
+        // Setting up vowel and digit collections:
+        List<char> vowels = new List<char>() { 'a', 'e', 'i', 'o', 'u' };
+        List<char> digits = new List<char>() { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+
+        // Reading input char:
+        char input = char.Parse(Console.ReadLine().ToLower());
+
+        // Checking if it belongs to any collection and print:
+        if (vowels.Contains(input) || digits.Contains(input))
+        {
+            if (vowels.Contains(input))
+            {
+                Console.WriteLine("vowel");
+            }
+            else
+            {
+                Console.WriteLine("digit");
+            }
+        }
+        else
+        {
+            Console.WriteLine("other");
+        }
+        
+    }
+}
