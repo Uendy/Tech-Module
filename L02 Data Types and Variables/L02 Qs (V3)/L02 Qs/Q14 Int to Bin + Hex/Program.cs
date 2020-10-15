26005ad769fbfc1da0eebe2e24c7be87c8124924
Uendy <alex.beshkov98@gmail.com>
2020-10-15 09:38:05 +0300

    Adding: -Q14 Converting Integer to Hexdec and Binary (Int to Bin + Hex)

---
+++ L02 Data Types and Variables/L02 Qs (V3)/L02 Qs/Q14 Int to Bin + Hex/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        // Instructions:Create a program to convert a decimal number to hexadecimal and binary number and print it.
+
+        // Example:
+        // Input:   Output:
+        //  10         A
+        //           1010
+
+        // Reading input:
+        int input = int.Parse(Console.ReadLine());
+
+        // Converting:
+        var hex = Convert.ToString(input, 16).ToUpper();
+        var bin = Convert.ToString(input, 2).ToUpper();
+
+        // Printing:
+        Console.WriteLine(hex);
+        Console.WriteLine(bin);
+
+    }
+}
---
+++ L02 Data Types and Variables/L02 Qs (V3)/L02 Qs/Q14 Int to Bin + Hex/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Q14 Int to Bin + Hex")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("Q14 Int to Bin + Hex")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("67de720c-6810-4057-b68e-6211dc1d0278")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
