830222db4b7850fd5d8f1f9dc5987109d1e2dee9
Uendy <alex.beshkov98@gmail.com>
2020-10-16 11:55:15 +0300

    Adding: -Q15 Prime Checker

---
+++ L02 Data Types and Variables/L02 Qs (V3)/L02 Qs/Q15 Prime Checker/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        // Instructions: You are given a program that checks if numbers in a given range [2...N] are prime. For each number is printed "{number} is prime -> {True or False}". The code however, is not very well written. Your job is to modify it in a way that is easy to read and understand.
+
+        // Example: Input:     Output:
+        //            5	       2 -> True
+        //                     3->True
+        //                     4->False
+        //                     5->True
+
+        // Reading input:
+        int input = int.Parse(Console.ReadLine());
+
+        // Cycling:
+        for (int i = 2; i <= input; i++)
+        {
+            bool isPrime = true;
+            for (int a = 2; a <= Math.Sqrt(i); a++)
+            {
+                if (i % a == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+
+            }
+
+            if (isPrime)
+            {
+                Console.WriteLine($"{i} -> True");
+            }
+            else
+            {
+                Console.WriteLine($"{i} -> False");
+            }
+        }
+    }
+}
