a24934d8464bd3581ace0d5a4141c01204764357
Uendy <alex.beshkov98@gmail.com>
2020-10-17 15:55:00 +0300

    Adding: -Q16 Comparing Floats (initial)

---
+++ L02 Data Types and Variables/L02 Qs (V3)/L02 Qs/Q16 Comparing Floats/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        // Instructions: Write a program that safely compares floating-point numbers (double) with precision eps = 0.000001. Note that we cannot directly compare two floating-point numbers a and b by a==b because of the nature of the floating-point arithmetic. Therefore, we assume two numbers are equal if they are more closely to each other than some fixed constant eps
+
+        // Example: 
+        // Number a      	Number b	        Equal (with precision eps=0.000001)         	                Explanation
+        //  5.3                6.01                         False                                   The difference of 0.71 is too big (> eps)
+
+    }
+}
