eb0ff1351880d2b7a7ad8a2611ddc7a359f9c95e
Uendy <alex.beshkov98@gmail.com>
2020-10-18 20:04:42 +0300

    Adding: -Q17 Printing ASCII

---
+++ L02 Data Types and Variables/L02 Qs (V3)/L02 Qs/Q17 Printing ASCII/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        // Instructions: Find online more information about ASCII (American Standard Code for Information Interchange) and write a program that prints part of the ASCII table of characters at the console.  On the first line of input you will receive the char index you should start with and on the second line - the index of the last character you should print.
+
+        // Example:
+        // Input          Output
+        //  60
+        //  65          < = > ? @ A
+
+
+        // Reading parameters:
+        int start = int.Parse(Console.ReadLine());
+        int end = int.Parse(Console.ReadLine());
+
+        // Cylcing and printing:
+        for (int i = start; i <= end; i++)
+        {
+            Console.Write($"{(char)i} ");
+        }
+
+        Console.WriteLine();
+    }
+}
