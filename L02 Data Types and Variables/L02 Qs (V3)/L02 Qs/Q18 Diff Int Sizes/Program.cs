f550181a29f0d35ac90d1b358c76036588544655
Uendy <alex.beshkov98@gmail.com>
2020-10-19 20:09:02 +0300

    Adding: -Q18 Diff Int Sizes (not complete yet) Svetlio told me to get used to forgetting things you learned, very freeing advice

---
+++ L02 Data Types and Variables/L02 Qs (V3)/L02 Qs/Q18 Diff Int Sizes/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        // Instructions: Given an input integer, you must determine which primitive data types are capable of properly storing that input.
+
+        // Input: 
+        // •	You receive N – integer which can be arbitrarily large or small
+
+        // Output:
+        // You must determine if the given primitives are capable of storing it.If yes, then print:
+        // { N} can fit in:
+        // *dataType
+
+        // If there is more than one appropriate data type, print each one on its own line and order them by size
+        // (sbyte < byte < short < ushort < int < uint < long).
+
+        // If the number cannot be stored in one of the four aforementioned primitives, print the line: 
+        // { N}  can't fit in any type
+
+        // Example: 
+        //   Input	                 Output
+        // 1500000000          1500000000 can fit in:
+        //                          *int
+        //                          * uint
+        //                          * long
+
+        // Example 2: 
+        //          Input                                                     Output
+        // 213333333333333333333333333333333333    213333333333333333333333333333333333 can't fit in any type
+
+        // Reading input:
+        string input = Console.ReadLine();
+
+        try
+        {
+            ulong inputParsed = ulong.Parse(input);
+        }
+        catch (Exception)
+        {
+            Console.WriteLine($"{input} can't fit in any type");
+            throw;
+        }
+    }
+}
