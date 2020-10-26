9d089bb4ecc017a0fee86cd07cf760ee556a75b5
Uendy <alex.beshkov98@gmail.com>
2020-10-26 18:42:23 +0200

    finished: -Q02 Min Method

--- L03 Methods, Debugging/L03 Qs (V3)/L03 Method Qs (V3)/Q02 Min Meth/Program.cs
+++ L03 Methods, Debugging/L03 Qs (V3)/L03 Method Qs (V3)/Q02 Min Meth/Program.cs
@@ -6,10 +6,25 @@ public class Program
         // Instructions: Create a method GetMin(int a, int b), that returns maximal of two numbers. Write a program that reads three numbers from the console and prints the biggest of them. Use the GetMax(…) method you just created.
 
         // Examples:
-        //Input  Output
-        //  1      3
+        // Input  Output
+        //  1       3
         //  2
         //  3    
 
+        // Reading inputs
+        int inputOne = int.Parse(Console.ReadLine());
+        int inputTwo = int.Parse(Console.ReadLine());
+        int inputThree = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(GetMax(inputOne, inputTwo, inputThree));
+    }
+    
+    /// returns the biggest of 3 inputs:
+    public static int GetMax(int inputOne, int inputTwo, int inputThree) 
+    {
+        int biggest = Math.Max(inputOne, inputTwo); 
+        biggest = Math.Max(biggest, inputThree);
+
+        return biggest;
     }
 }
