1b57bc05c8211d115a5c30ab7ccb91c354edc53c
Uendy <alex.beshkov98@gmail.com>
2020-10-26 18:35:44 +0200

    Adding: -start of Q02 Min Method (Min Meth)

---
+++ L03 Methods, Debugging/L03 Qs (V3)/L03 Method Qs (V3)/Q02 Min Meth/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        // Instructions: Create a method GetMin(int a, int b), that returns maximal of two numbers. Write a program that reads three numbers from the console and prints the biggest of them. Use the GetMax(…) method you just created.
+
+        // Examples:
+        //Input  Output
+        //  1      3
+        //  2
+        //  3    
+
+    }
+}
