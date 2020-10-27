740d45b4723f9bf6f400628ff045ed44bbe9bce1
Uendy <alex.beshkov98@gmail.com>
2020-10-27 17:33:17 +0200

    Adding: -Q04 Rev Num (Numbers in Reversed Order) start

---
+++ L03 Methods, Debugging/L03 Qs (V3)/L03 Method Qs (V3)/Q04 Rev Num/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        // Instructions: Write a method that prints the digits of a given decimal number in a reversed order.
+        // Example:
+        // Input	Output
+        //  256      652
+        //  1.12	 21.1
+
+    }
+}
