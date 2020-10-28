3e2a6ab3bd2853fd660c6cecc7ebccaec73974bb
Uendy <alex.beshkov98@gmail.com>
2020-10-28 19:12:44 +0200

    Update

--- L03 Methods, Debugging/L03 Qs (V3)/L03 Method Qs (V3)/Q06 Prime/Program.cs
+++ L03 Methods, Debugging/L03 Qs (V3)/L03 Method Qs (V3)/Q06 Prime/Program.cs
@@ -19,5 +19,26 @@ public class Program
         //    6737626471            true
         //    117342557809          false
 
+        // Reading input:
+        long input = long.Parse(Console.ReadLine());
+        bool isPrime = PrimeChecker(input);
+
+        // Printing output:
+        Console.WriteLine(isPrime);
+    }
+
+    /// Method to check if input is prime
+    private static bool PrimeChecker(long input)
+    {
+        bool isPrime = true;
+        for (int i = 2; i < Math.Sqrt(input); i++) // cycles through 2 -> sqrt(N) (any higher num is not necessary to check)
+        {
+            if (input % i == 0) // see if the num is divisible
+            {
+                isPrime = false;
+            }
+        }
+
+        return isPrime;
     }
 }
