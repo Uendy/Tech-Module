182dd842dad575327dd9f1f66c66d44a6cbcd192
Uendy <alex.beshkov98@gmail.com>
2020-10-28 19:07:07 +0200

    Adding: -Q06 Prime(Prime Checker) Start

---
+++ L03 Methods, Debugging/L03 Qs (V3)/L03 Method Qs (V3)/Q06 Prime/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        // Instructions: Write a Boolean method IsPrime(n) that check whether a given integer number n is prime. 
+
+        // Example:
+        // Input                   Output
+        //n	IsPrime(n)
+        //    0                     false
+        //    1                     false
+        //    2                     true
+        //    3                     true
+        //    4                     false
+        //    5                     true
+        //    323                   false
+        //    337                   true
+        //    6737626471            true
+        //    117342557809          false
+
+    }
+}
