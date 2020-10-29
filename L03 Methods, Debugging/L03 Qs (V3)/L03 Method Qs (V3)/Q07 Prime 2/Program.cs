6675181434a21f4692263a280553c9fd50ba1159
Uendy <alex.beshkov98@gmail.com>
2020-10-29 14:36:42 +0200

    Adding: -Q07 Prime 2 (Primes in Given Range) Start

---
+++ L03 Methods, Debugging/L03 Qs (V3)/L03 Method Qs (V3)/Q07 Prime 2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+public class Program
+{
+    public static void Main()
+    {
+        // Instructions: Write a method that calculates all prime numbers in given range and returns them as list of integers:
+        // Template: static List<int> FindPrimesInRange(startNum, endNum)
+        // Write a method to print a list of integers.Write a program that enters two integer numbers(each at a separate line) and prints all primes in their range, separated by a comma.
+
+        // Example:
+        // Input: (Start and End Number)              Output
+        //  0 10                                    2, 3, 5, 7
+        //  5 11                                     5, 7, 11
+    }
+}
