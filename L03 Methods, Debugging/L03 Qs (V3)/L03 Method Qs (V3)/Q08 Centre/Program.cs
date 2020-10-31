d121fcafbb8e48bcbdce6c2a03e8b88cc1774ef4
Uendy <alex.beshkov98@gmail.com>
2020-10-31 18:11:49 +0200

    Update: -finished Q08 Centre

--- L03 Methods, Debugging/L03 Qs (V3)/L03 Method Qs (V3)/Q08 Centre/Program.cs
+++ L03 Methods, Debugging/L03 Qs (V3)/L03 Method Qs (V3)/Q08 Centre/Program.cs
@@ -11,5 +11,32 @@ public class Program
         // - 1
         // 2
 
+        // Reading input:
+        int x1 = int.Parse(Console.ReadLine());
+        int y1 = int.Parse(Console.ReadLine());
+
+        int x2 = int.Parse(Console.ReadLine());
+        int y2 = int.Parse(Console.ReadLine());
+
+        // Method and ouput
+        ClosestPoints(x1, y1, x2, y2);
+
+    }
+
+    /// Method that finds distance to center and prints result
+    public static void ClosestPoints(int x1, int y1, int x2, int y2)
+    {
+        // Find distances from center, smaller distance = closer
+        int pointOneDistnace = Math.Abs(x1) + Math.Abs(y1);
+        int pointTwoDistance = Math.Abs(x2) + Math.Abs(y2);
+
+        if (pointOneDistnace <= pointTwoDistance) // if p1 is smaller or equal to p2 we print it
+        {
+            Console.WriteLine($"({x1}, {y1})");
+        }
+        else
+        {
+            Console.WriteLine($"({x2}, {y2})");
+        }
     }
 }
