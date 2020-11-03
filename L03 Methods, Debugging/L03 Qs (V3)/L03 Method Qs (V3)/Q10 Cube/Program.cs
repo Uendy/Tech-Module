f084a780bb0d435451fda57f359ab3a0130b3344
Uendy <alex.beshkov98@gmail.com>
2020-11-03 20:04:21 +0200

    Update: -Adding Q10 Cube (Cube Properties) Start

---
+++ L03 Methods, Debugging/L03 Qs (V3)/L03 Method Qs (V3)/Q10 Cube/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        // Instructions: Write a program that can calculate the length of the face diagonals, space diagonals, volume and surface area of a cube (http://www.mathopenref.com/cube.html) by a given side. On the first line you will get the side of the cube. On the second line is given the parameter (face, space, volume or area).
+        // Output should be rounded to the second digit after the decimal point:
+
+        // Example:
+        // Input	Output
+        //  5        7.07
+        // face
+
+        // Example 2:
+        // Input     Output
+        //   5       125.00
+        // Volume
+    }
+}
