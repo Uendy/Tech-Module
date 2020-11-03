a932464e8ccf66a7864f952ba2fccd1eec5e3127
Uendy <alex.beshkov98@gmail.com>
2020-11-03 19:34:40 +0200

    Update: -finished Q09

--- L03 Methods, Debugging/L03 Qs (V3)/L03 Method Qs (V3)/Q09 Line/Program.cs
+++ L03 Methods, Debugging/L03 Qs (V3)/L03 Method Qs (V3)/Q09 Line/Program.cs
@@ -31,8 +31,19 @@ public class Program
         int x4 = int.Parse(Console.ReadLine());
         int y4 = int.Parse(Console.ReadLine());
 
+        // Getting line lengths:
         double line1Length = FindLength(x1, y1, x2, y2);
         double line2Length = FindLength(x3, y3, x4, y4);
+
+        // Comparing lengths, then comparing points, then printing
+        if (line1Length >= line1Length) // if l1 is bigger or equal than l2 use points from l1
+        {
+            ClosestPoints(x1, y1, x2, y2);
+        }
+        else
+        {
+            ClosestPoints(x3, y3, x4, y4);
+        }
     }
 
     /// Find length of given line via pythagoras theorem
@@ -44,5 +55,22 @@ public class Program
         double length = Math.Sqrt(Math.Pow(xDiff, 2) + Math.Pow(yDiff, 2));
 
         return length;
+    }
+
+    /// Find closer point to center:
+    public static void ClosestPoints(int x1, int y1, int x2, int y2)
+    {
+        // Find distances from center, smaller distance = closer
+        int pointOneDistnace = Math.Abs(x1) + Math.Abs(y1);
+        int pointTwoDistance = Math.Abs(x2) + Math.Abs(y2);
+
+        if (pointOneDistnace <= pointTwoDistance) // if p1 is smaller or equal to p2 we print it
+        {
+            Console.WriteLine($"({x1}, {y1})");
+        }
+        else
+        {
+            Console.WriteLine($"({x2}, {y2})");
+        }
     }
 }
