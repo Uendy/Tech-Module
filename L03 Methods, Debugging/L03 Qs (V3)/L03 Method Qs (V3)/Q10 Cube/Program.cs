907b7844a205327744f24ba8998eff2535eeee9a
Uendy <alex.beshkov98@gmail.com>
2020-11-03 20:18:42 +0200

    Update: -finished Q10 Cube

--- L03 Methods, Debugging/L03 Qs (V3)/L03 Method Qs (V3)/Q10 Cube/Program.cs
+++ L03 Methods, Debugging/L03 Qs (V3)/L03 Method Qs (V3)/Q10 Cube/Program.cs
@@ -15,5 +15,54 @@ public class Program
         // Input     Output
         //   5       125.00
         // Volume
+
+        // Reading input:
+        int side = int.Parse(Console.ReadLine());
+        string function = Console.ReadLine();
+
+        // Reroute to method via functions
+        switch (function)
+        {
+            case "face":
+                double face = FindFace(side);
+                Console.WriteLine(face);
+                    break;
+
+            case "space":
+                double space = FindSpace(side);
+                Console.WriteLine(space);
+                    break;
+
+            case "volume":
+                double volume = FindVolume(side);
+                Console.WriteLine(volume);
+                break;
+
+            case "area":
+                double area = FindArea(side);
+                Console.WriteLine(area);
+                break;
+        }
+    }
+     public static double FindFace(int side)
+     {
+        double face = Math.Sqrt(2 * Math.Pow(side, 2));
+        return Math.Round(face, 2);
+     }
+    public static double FindSpace(int side)
+    {
+        double space = Math.Sqrt(3 * Math.Pow(side, 2));
+        return Math.Round(space, 2);
+    }
+    public static double FindVolume(int side)
+    {
+        double volume = Math.Pow(side, 3);
+        return Math.Round(volume, 2);
+    }
+
+    public static double FindArea(int side)
+    {
+        double area = 6 * Math.Pow(side, 2);
+        return Math.Round(area, 2);
     }
 }
