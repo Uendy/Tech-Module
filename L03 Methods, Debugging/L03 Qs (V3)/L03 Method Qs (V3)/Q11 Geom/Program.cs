c5977ab6fa61009d606bfa613e1d1dfa7e4f3b26
Uendy <alex.beshkov98@gmail.com>
2020-11-04 19:19:19 +0200

    Adding: -Q11 Geom (Geometry Calculator) Start

---
+++ L03 Methods, Debugging/L03 Qs (V3)/L03 Method Qs (V3)/Q11 Geom/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        // Instructions: Write a program that can calculate the area of four different geometry figures - triangle, square, rectangle and circle.
+        // On the first line you will get the figure type. Next you will get parameters for the chosen figure, each on a different line:
+        // •	Triangle - side and height
+        // •	Square - side
+        // •	Rectangle - width and height
+        // •	Circle - radius
+        // The output should be rounded to the second digit after the decimal point:
+
+        // Example: 
+        // Input	   Output
+        // triangle     9.00
+        //   3
+        //   6   
+
+    }
+}
