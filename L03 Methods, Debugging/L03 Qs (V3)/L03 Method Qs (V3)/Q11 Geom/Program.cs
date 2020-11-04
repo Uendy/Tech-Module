4c0523321fa4c09e6e9a1e588c4ce2f862e684e1
Uendy <alex.beshkov98@gmail.com>
2020-11-04 19:35:16 +0200

    Update: -finished Q11

--- L03 Methods, Debugging/L03 Qs (V3)/L03 Method Qs (V3)/Q11 Geom/Program.cs
+++ L03 Methods, Debugging/L03 Qs (V3)/L03 Method Qs (V3)/Q11 Geom/Program.cs
@@ -17,5 +17,56 @@ public class Program
         //   3
         //   6   
 
+        // Reading input:
+        string shape = Console.ReadLine().ToLower();
+
+        switch (shape) // check which shape, the rest is pretty explanatory: read following inputs, method to calc subsequent area, round and print
+        {
+            case "triangle":
+                int triSide = int.Parse(Console.ReadLine());
+                int triHeight = int.Parse(Console.ReadLine());
+                double triArea = CalcTriangleArea(triSide, triHeight);
+                Console.WriteLine($"{Math.Round(triArea, 2):f2}");
+                break;
+
+            case "square":
+                int SqSide = int.Parse(Console.ReadLine());
+                double sqArea = CalcSquareArea(SqSide);
+                Console.WriteLine($"{Math.Round(sqArea, 2):f2}");
+                break;
+
+            case "rectangle":
+                int reWidth = int.Parse(Console.ReadLine());
+                int reHeight = int.Parse(Console.ReadLine());
+                double reArea = CalcRectangleArea(reWidth, reHeight);
+                Console.WriteLine($"{Math.Round(reArea, 2):f2}");
+                break;
+
+            case "circle":
+                int ciRadius = int.Parse(Console.ReadLine());
+                double ciArea = CalcCircleArea(ciRadius);
+                Console.WriteLine($"{Math.Round(ciArea, 2):f2}");
+                break;
+        }
+    }
+    public static double CalcTriangleArea(int triSide, int triHeight)
+        {
+            double area = (triSide * triHeight) / 2;
+            return area;
+        }
+    public static double CalcSquareArea(int sqSide)
+    {
+        double area = sqSide * sqSide;
+        return area;
+    }
+    public static double CalcRectangleArea(int reWidth, int reHeight)
+    {
+        double area = reHeight * reHeight;
+        return area;
+    }
+    public static double CalcCircleArea(int ciRadius)
+    {
+        double area = Math.PI * Math.Pow(ciRadius, 2);
+        return area;
     }
 }
