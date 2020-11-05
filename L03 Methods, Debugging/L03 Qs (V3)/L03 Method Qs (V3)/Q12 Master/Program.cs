4654941031dff6c082f1a94ae11a70780a0dd2b1
Uendy <alex.beshkov98@gmail.com>
2020-11-05 18:58:13 +0200

    Update: -added formal code, only the content (methods left)

--- L03 Methods, Debugging/L03 Qs (V3)/L03 Method Qs (V3)/Q12 Master/Program.cs
+++ L03 Methods, Debugging/L03 Qs (V3)/L03 Method Qs (V3)/Q12 Master/Program.cs
@@ -18,5 +18,40 @@ public class Program
         //           464
         //           545
 
+        // Reading input:
+        int finalNum = int.Parse(Console.ReadLine());
+
+        // List of master numbers:
+        var masterNums = new List<int>();
+
+        // Cycle through 1...finalNum and check if its master num
+        for (int i = 1; i <= finalNum; i++)
+        {
+            bool isSymmetric = CheckSymmetry(i); // Is symmetric(palindrome), e.g. 5, 77, 282, 14341, 9553559.
+            if (isSymmetric == false)
+            {
+                continue;
+            }
+
+            bool isDivisibleBySeven = CheckDivisibility(i); // Its sum of digits is divisible by 7, e.g. 77, 313, 464, 5225, 37173.
+            if (isDivisible == false)
+            {
+                continue;
+            }
+
+            bool hasEvenDigit = CheckForEvenDigit(i); // Holds at least one even digit, e.g. 232, 707, 6886, 87578.
+
+            bool isMasterNum = isSymmetric && isDivisibleBySeven && hasEvenDigit; // If it passes all checks add it to list of master nums
+            if (isMasterNum)
+            {
+                masterNums.Add(i);
+            }
+
+            // Printing output:
+            foreach (var item in masterNums)
+            {
+                Console.WriteLine(item);
+            }
+        }
     }
 }
