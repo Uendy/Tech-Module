6e8352f38f5c7a80fb31ea2a067faf6d4a8e90a2
Uendy <alex.beshkov98@gmail.com>
2020-11-05 18:47:57 +0200

    Adding: -Q12 Master (Master Numbers) Start

---
+++ L03 Methods, Debugging/L03 Qs (V3)/L03 Method Qs (V3)/Q12 Master/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+public class Program
+{
+    public static void Main()
+    {
+        // Instructions:
+        // A master number is an integer that holds the following properties:
+        // •	Is symmetric(palindrome), e.g. 5, 77, 282, 14341, 9553559.
+        // •	Its sum of digits is divisible by 7, e.g. 77, 313, 464, 5225, 37173.
+        // •	Holds at least one even digit, e.g. 232, 707, 6886, 87578.
+        // Write a program to print all master numbers in the range[1…n].
+
+        // Example:
+        // Input	Output
+        //  600      232
+        //           383
+        //           464
+        //           545
+
+    }
+}
