2bd5c29b65cd4791b03debbe164d3e184774fb99
Uendy <alex.beshkov98@gmail.com>
2020-11-07 18:18:24 +0200

    Adding: - Q13 Factoria (Factorial) Start - Also added reference to System.Numerics for BigInt

---
+++ L03 Methods, Debugging/L03 Qs (V3)/L03 Method Qs (V3)/Q13 Factoria/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        // Instructions: Write a program that calculates and prints the n! for any n in the range [1…1000].
+
+        // Example:
+        // Input	                                                                            Output
+        // 100   93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000
+
+    }
+}
