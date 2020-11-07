fd7ef6d074319b5fd0ed4068ff922f29b132e08b
Uendy <alex.beshkov98@gmail.com>
2020-11-07 18:32:46 +0200

    Adding: -Q14 Trail (Factorial Trailing Zeroes) Start

---
+++ L03 Methods, Debugging/L03 Qs (V3)/L03 Method Qs (V3)/Q14 Trailing/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        #region
+        // Instructions: Create a program that counts the trailing zeroes of the factorial of a given number.
+
+        // Example: 
+        // Input    Output                                                                                              Comment
+        //  100       24     100! = 93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000-> 24 trailing zeroes
+        #endregion
+    }
+}
