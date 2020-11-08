00bd597f9d80eaee0de061c5da3c6962baabb074
Uendy <alex.beshkov98@gmail.com>
2020-11-08 18:24:43 +0200

    Update: -did most of Q01, but my backwards for cycle dosen't work for string[] of different lengths

--- L04 Arrays/L04 Qs (V3)/Array Qs (V3)/Q01 Largest Common End/Program.cs
+++ L04 Arrays/L04 Qs (V3)/Array Qs (V3)/Q01 Largest Common End/Program.cs
@@ -9,9 +9,56 @@ public class Program
 
         // Example: 
         //              Input	                             Output                      	      Comments
-       //  hi php java csharp sql html css js                  3               The largest common end is at the left: hi php java
+        //  hi php java csharp sql html css js                  3               The largest common end is at the left: hi php java
         // hi php java js softuni nakov java learn    
 
         #endregion
+
+        // Reading input:
+        var firstInput = Console.ReadLine().Split(' ');
+        var secondInput = Console.ReadLine().Split(' ');
+
+        // Finding smaller length
+        int smallerLength = Math.Min(firstInput.Length, secondInput.Length);
+
+        // Cycling front to back
+        int matchingStartCount = 0;
+        for (int i = 0; i < smallerLength; i++)
+        {
+            matchingStartCount = CompareAndIncrament(matchingStartCount, i, firstInput, secondInput);
+        }
+
+        // Cycling beck to front
+        int matchingEndCount = 0;
+        for (int i = smallerLength - 1; i >= 0; i--)
+        {
+            matchingEndCount = CompareAndIncrament(matchingEndCount, i, firstInput, secondInput);
+        }
+
+        // Finding longest count and Printing Output:
+        if (matchingStartCount >= matchingEndCount)
+        {
+            Console.WriteLine(matchingStartCount);
+        }
+        else
+        {
+            Console.WriteLine(matchingEndCount);
+        }
+
+    }
+
+    /// Gets the current string from both arrays, then checks to see if they match and incraments count
+    public static int CompareAndIncrament(int count, int i, string[] firstInput, string[] secondInput)
+    {
+        string firstInputString = firstInput[i];
+        string secondInputString = secondInput[i];
+
+        bool isMatch = firstInputString == secondInputString;
+        if (isMatch)
+        {
+            count++;
+        }
+
+        return count;
     }
 }
