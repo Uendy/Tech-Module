b6a5aeb0f6ad02a24a778d42eac73f39b8aa70d8
Uendy <alex.beshkov98@gmail.com>
2020-11-08 17:47:15 +0200

    Creating: -L04 (Arrays) Qs (V3) folder: -- Q01 Largest Common End (Start) -- Array Exercises.docx

---
+++ L04 Arrays/L04 Qs (V3)/Array Qs (V3)/Q01 Largest Common End/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        #region
+        // Instructions:
+        // Read two arrays of words and find the length of the largest common end (left or right).
+
+        // Example: 
+        //              Input	                             Output                      	      Comments
+       //  hi php java csharp sql html css js                  3               The largest common end is at the left: hi php java
+        // hi php java js softuni nakov java learn    
+
+        #endregion
+    }
+}
