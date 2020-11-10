3b97cc059e98b66bdec3f889a552a46f4c93bf88
Uendy <alex.beshkov98@gmail.com>
2020-11-10 17:47:04 +0200

    Adding: -Q02 Rotate and Sum (Start)

---
+++ L04 Arrays/L04 Qs (V3)/Array Qs (V3)/Q02 Rotate and Sum/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        #region
+        // Instructions: To “rotate an arrTo “rotate an array on the right” means to move its last element first: {1, 2, 3}  {3, 1, 2}.
+        // Write a program to read an array of n integers(space separated on a single line) and an integer k, rotate the array right k times and sum the obtained arrays after each rotation as shown below. ay on the right” means to move its last element first: {1, 2, 3}  {3, 1, 2}.
+        // Write a program to read an array of n integers(space separated on a single line) and an integer k, rotate the array right k times and sum the obtained arrays after each rotation as shown below.
+
+        // Example:
+        //    Input:             Output:            Comment:
+        //  3 2 4 - 1            3 2 5 6            rotated1[] = -1  3  2  4
+        //     2                                    rotated2[] =  4 -1  3  2
+        //                                               sum[] =  3  2  5  6
+
+        #endregion
+    }
+}
