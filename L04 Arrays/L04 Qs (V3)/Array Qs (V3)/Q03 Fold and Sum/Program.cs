a94cd057f5350a2b4bded239a5734717e3dcf0d1
Uendy <alex.beshkov98@gmail.com>
2020-11-11 18:46:05 +0200

    Adding: -Q03 Fold and Sum (Start)

---
+++ L04 Arrays/L04 Qs (V3)/Array Qs (V3)/Q03 Fold and Sum/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        #region
+        // Instructions: Read an array of 4*k integers, fold it like shown below, and print the sum of the upper and lower two rows (each holding 2 * k integers):
+        // Example: 
+        //   Input	        Output	            Comments
+        //  5 2 3 6          7 9                 5  6 +
+        //                                       2  3 =
+        //                                       7  9
+        #endregion
+
+
+    }
+}
