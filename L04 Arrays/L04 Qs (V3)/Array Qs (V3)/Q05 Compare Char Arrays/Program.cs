e03f09d24293e3e6f32e4c50d9f097b780ee4626
Uendy <alex.beshkov98@gmail.com>
2020-11-14 19:41:42 +0200

    Adding: -Q05 Compare Char Arrays (Start)

---
+++ L04 Arrays/L04 Qs (V3)/Array Qs (V3)/Q05 Compare Char Arrays/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        #region
+        // Instructions: 
+        // Compare two char arrays lexicographically(letter by letter).
+        // Print the them in alphabetical order, each on separate line.
+
+        // Example:
+        // Input            Output
+        //p e t e r         annie
+        // a n n i e        peter
+
+        // Example 2:
+        // Input            Ouput
+        // a n n i e         an
+        // a n              annie
+        #endregion
+    }
+}
