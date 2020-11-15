8557ed90e1aabf4e72360499e3013c9b6a5e3686
Uendy <alex.beshkov98@gmail.com>
2020-11-15 20:15:30 +0200

    Update: -Finished: Q05

--- L04 Arrays/L04 Qs (V3)/Array Qs (V3)/Q05 Compare Char Arrays/Program.cs
+++ L04 Arrays/L04 Qs (V3)/Array Qs (V3)/Q05 Compare Char Arrays/Program.cs
@@ -18,5 +18,22 @@ public class Program
         // a n n i e         an
         // a n              annie
         #endregion
+
+        // Reading input:
+        var first = string.Join("", Console.ReadLine().Split(' '));
+        var second = string.Join("", Console.ReadLine().Split(' '));;
+
+        // compare them with the method and print depending on the retunred value
+        int compared = string.Compare(first, second);
+        if (compared <= 0)
+        {
+            Console.WriteLine(first);
+            Console.WriteLine(second);
+        }
+        else
+        {
+            Console.WriteLine(second);
+            Console.WriteLine(first);
+        }
     }
 }
