59cfe3cba056824d0b000e418386e6d9863c30c9
Uendy <alex.beshkov98@gmail.com>
2020-11-16 19:03:42 +0200

    Update

--- L04 Arrays/L04 Qs (V3)/Array Qs (V3)/Q06 Max Seq of Equal/Program.cs
+++ L04 Arrays/L04 Qs (V3)/Array Qs (V3)/Q06 Max Seq of Equal/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 public class Program
 {
     public static void Main()
@@ -11,5 +12,47 @@ public class Program
         //      4 4 4 4                     4 4 4 4
         // 0 1 1 5 2 2 6 3 3                  1 1
         #endregion
+
+        // Reading input:
+        var array = Console.ReadLine().Split(' ').Select(char.Parse).ToArray();
+
+        // basically go from righ to left, and stop when i != i + 1 || i < array.Length - 2(as it goes to +1)
+        // if the num is strictly bigger than currentMax then change them and have a var for which num it is, then print it * max
+
+        // Set up variables
+        var maxSequence = 0;
+        var element = ' ';
+        var currentSequence = 0;
+
+        // cycle right:
+        for (int index = 0; index < array.Count() - 1; index++)
+        {
+            char current = array[index];
+            char next = array[index + 1];
+
+            if (current == next)
+            {
+                currentSequence++; 
+            }
+            else // not sequence
+            {
+                if (currentSequence > maxSequence)
+                {
+                    maxSequence = currentSequence; // updating high score
+                    element = current;
+                }
+                currentSequence = 0; // annuling highscore
+            }
+        }
+
+        // last check to see if with the last cycle we hit a new record:
+        if (currentSequence > maxSequence)
+        {
+            maxSequence = currentSequence; // updating high score
+            element = array[array.Length - 1];
+        }
+
+        // Printing output
+        Console.WriteLine(new string(element, maxSequence));
     }
 }
