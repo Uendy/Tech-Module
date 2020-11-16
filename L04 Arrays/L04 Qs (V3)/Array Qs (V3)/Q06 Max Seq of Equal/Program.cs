4c9bd33ae9b4a2c8f153123e4a86596007174c9b
Uendy <alex.beshkov98@gmail.com>
2020-11-16 18:35:11 +0200

    Adding: -Q06 Max Seq of Equal (Max Sequence of Equal Elements) Start

---
+++ L04 Arrays/L04 Qs (V3)/Array Qs (V3)/Q06 Max Seq of Equal/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        #region
+        // Instructions: Write a program that finds the longest sequence of equal elements in an array of integers. If several longest sequences exist, print the leftmost one.
+        //       Input	                     Output
+        // 2 1 1 2 3 3 2 2 2 1               2 2 2
+        //   1 1 1 2 3 1 3 3                 1 1 1
+        //      4 4 4 4                     4 4 4 4
+        // 0 1 1 5 2 2 6 3 3                  1 1
+        #endregion
+    }
+}
