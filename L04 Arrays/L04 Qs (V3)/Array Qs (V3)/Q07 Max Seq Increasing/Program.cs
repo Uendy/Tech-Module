77d8cabb4bc2d7e1d952e9afea79380a5fe6959d
Uendy <alex.beshkov98@gmail.com>
2020-11-18 18:52:43 +0200

    Adding: -Q07 Max Seq Increasing (Max Sequence of Increasing Elements) start

---
+++ L04 Arrays/L04 Qs (V3)/Array Qs (V3)/Q07 Max Seq Increasing/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        #region
+        // Instructions: Write a program that finds the longest increasing subsequence in an array of integers. The longest increasing subsequence is a portion of the array (subsequence) that is strongly increasing and has the longest possible length. If several such subsequences exist, find the left most of them.
+        // Example:
+        //    Input          Output
+        // 3 2 3 4 2 2 4    2 3 4
+        // 4 5 1 2 3 4 5    1 2 3 4 5
+        // 3 4 5 6          3 4 5 6
+        // 0 1 1 2 2 3 3    0 1
+        #endregion
+
+        // Reading input:
+
+    }
+}
