5e4bc3f97f3a88f4314056bfdccbead889e347b0
Uendy <alex.beshkov98@gmail.com>
2020-11-20 15:38:06 +0200

    Update: -finished Q08 (Using Dictionary)

--- L04 Arrays/L04 Qs (V3)/Array Qs (V3)/Q08 Most Frequent Num/Program.cs
+++ L04 Arrays/L04 Qs (V3)/Array Qs (V3)/Q08 Most Frequent Num/Program.cs
@@ -14,8 +14,31 @@ public class Program
         //         input                output
         // 2 2 2 2 1 2 2 2	              2
         // 7 7 7 0 2 2 2 0 10 10 10       7
-
         #endregion
 
+        // Reading input:
+        var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        var dictionary = new Dictionary<int, int>();
+
+        // Cycling and filling dictionary
+        for (int index = 0; index < input.Length; index++)
+        {
+            int currentNum = input[index];
+
+            bool newNum = !dictionary.ContainsKey(currentNum);
+            if (newNum) // add a new kvp to dictionary
+            {
+                dictionary[currentNum] = 1;
+            }
+            else // incrament current kvp
+            {
+                dictionary[currentNum]++;
+            }
+        }
+
+        // finding most frequent num
+        var output = dictionary.OrderByDescending(x => x.Value).First().Key;
+        Console.WriteLine(output);
     }
 }
