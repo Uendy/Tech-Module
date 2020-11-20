a1586488f58493fdb2c84c1dd17408f1393f9d75
Uendy <alex.beshkov98@gmail.com>
2020-11-20 14:29:39 +0200

    Adding: -Q08 Most Frequent Num (Most Frequent Number) start

---
+++ L04 Arrays/L04 Qs (V3)/Array Qs (V3)/Q08 Most Frequent Num/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        #region
+        // Instructions: Write a program that finds the most frequent number in a given sequence of numbers. 
+        //•	Numbers will be in the range[0…65535].
+        //•	In case of multiple numbers with the same maximal frequency, print the leftmost of them.
+
+        // Example:
+        //         input                output
+        // 2 2 2 2 1 2 2 2	              2
+        // 7 7 7 0 2 2 2 0 10 10 10       7
+
+        #endregion
+
+    }
+}
