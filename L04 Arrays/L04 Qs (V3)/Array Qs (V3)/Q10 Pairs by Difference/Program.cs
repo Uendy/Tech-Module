2bd89e69190a024d73c2eb4ea905c252afc80c2c
Uendy <alex.beshkov98@gmail.com>
2020-11-21 18:53:14 +0200

    - Adding: Q10 Pairs by Difference (Start)

---
+++ L04 Arrays/L04 Qs (V3)/Array Qs (V3)/Q10 Pairs by Difference/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+    public class Program
+    {
+        public static void Main()
+        {
+        #region
+        // Instructions: Write a program that count the number of pairs in given array which difference is equal to given number.
+        //          •	The first line holds the sequence of numbers.
+        //          •	The second line holds the difference.
+
+        // Example:
+        //  Input        Output                                    Explanation
+        // 1 5 3 4 2      3               Pairs of elements with difference 2-> { 1, 3}, { 5, 3}, { 4, 2}
+        //    2   
+        5 3 8 10 12 1
+1   0   No pairs with difference 1
+
+        #endregion
+    }
+}
