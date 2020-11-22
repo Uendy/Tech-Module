cb57a0e6cc3e2db29e8487d31798f77028bbce52
Uendy <alex.beshkov98@gmail.com>
2020-11-22 17:00:29 +0200

    Adding: -Q11 Equal Sums

---
+++ L04 Arrays/L04 Qs (V3)/Array Qs (V3)/Q11 Equal Sums/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        #region
+        // Instructions: Write a program that determines if there exists an element in the array such that the sum of the elements on its left is equal to the sum of the elements on its right. If there are no elements to the left / right, their sum is considered to be 0. Print the index that satisfies the required condition or “no” if there is no such index.
+
+        // Example:
+        //  Input        Output                Explanation
+        // 1 2 3 3	        2	    At a[2] -> left sum = 3, right sum = 3
+        //                                a[0] + a[1] = a[3]
+        #endregion
+
+
+    }
+}
