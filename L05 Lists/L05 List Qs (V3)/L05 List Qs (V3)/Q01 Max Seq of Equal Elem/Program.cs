8c88ce74df6e9abdf4f9db3e5be8444df7d3b4b2
Uendy <alex.beshkov98@gmail.com>
2020-11-23 17:07:02 +0200

    Big Update: -Q01, just need to fix the last index being added to sequence

--- L05 Lists/L05 List Qs (V3)/L05 List Qs (V3)/Q01 Max Seq of Equal Elem/Program.cs
+++ L05 Lists/L05 List Qs (V3)/L05 List Qs (V3)/Q01 Max Seq of Equal Elem/Program.cs
@@ -11,5 +11,43 @@ public class Program
         //    Input            Output
         //7 7 4 4 5 5 3 3    	7 7
         #endregion
+
+        // Reading input:
+        var input = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+        // Initialize output list:
+        var maxSeq = new List<int>();
+
+        // Cycle and check:
+        for (int index = 0; index < input.Count(); index++)
+        {
+            var currentSeq = new List<int>();
+
+            int currentNum = input[index];
+
+            // Cycle and add all continuous same numbers:
+            for (int j = index+1; j < input.Count(); j++)
+            {
+                int nextNum = input[j];
+
+                currentSeq.Add(currentNum);
+
+                if (currentNum != nextNum) // break this cycle
+                {
+                    break;
+                }
+            }
+
+            // Check if the seq is greater than the current maxSeq
+            bool newHighScore = currentSeq.Count() > maxSeq.Count();
+            if (newHighScore)
+            {
+                maxSeq = new List<int>(currentSeq.ToList());
+                currentSeq.Clear();
+            }
+        }
+
+        // Printing output:
+        Console.WriteLine(string.Join(" ", maxSeq));
     }
 }
