c4e7b0e4709e01288779563e409e165665f4124b
Uendy <alex.beshkov98@gmail.com>
2020-11-23 17:19:13 +0200

    Adding: -Q02 Change List (Start)

---
+++ L05 Lists/L05 List Qs (V3)/L05 List Qs (V3)/Q02 Change List/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        #region
+        // Instructions: Write a program, which reads a list of integers from the console and receives commands, which manipulate the list. Your program may receive the following commands: 
+        //  •	Delete { element} – delete all elements in the array, which are equal to the given element
+        //  •	Insert { element} { position} – insert element and the given position
+        // You should stop the program when you receive the command Odd or Even.If you receive Odd  print all odd numbers in the array separated with single whitespace, otherwise print the even numbers.
+
+        // Example: 
+        //     Input                Output
+        // 1 2 3 4 5 5 5 6           1 3
+        //  Delete 5
+        //  Insert 10 1
+        //  Delete 5
+        //  Odd
+        #endregion
+
+
+    }
+}
