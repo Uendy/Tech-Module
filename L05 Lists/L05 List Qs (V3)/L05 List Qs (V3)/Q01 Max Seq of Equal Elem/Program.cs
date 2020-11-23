34d7e42b7e4b408830fc665e8bec01dc6f31ec06
Uendy <alex.beshkov98@gmail.com>
2020-11-23 16:42:28 +0200

    Adding: - List Qs (V3) folder: -- List exercises.docx -- List Qs (V3).sln: --- Q01 Max Sequence of Equal Elem (Max Sequence of Equal Elements) Start

---
+++ L05 Lists/L05 List Qs (V3)/L05 List Qs (V3)/Q01 Max Seq of Equal Elem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        #region
+        // Instructions: Read a list of integers and find the longest sequence of equal elements. If several exist, print the leftmost.
+        // Example: 
+        //    Input            Output
+        //7 7 4 4 5 5 3 3    	7 7
+        #endregion
+    }
+}
