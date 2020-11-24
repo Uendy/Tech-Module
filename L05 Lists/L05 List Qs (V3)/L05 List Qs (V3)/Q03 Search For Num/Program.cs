e96c26928736ef057954f1c5539f281aabcdb0a8
Uendy <alex.beshkov98@gmail.com>
2020-11-24 15:49:16 +0200

    -Adding: Q03 Search For Num (Search For Number) Start

---
+++ L05 Lists/L05 List Qs (V3)/L05 List Qs (V3)/Q03 Search For Num/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        #region
+        // Instructions: On the first line, you will receive a list of integers. On the next line, you will receive an array with exactly three numbers. First number represents the number of elements you have to take from the list (starting from the first one). Second number represents the number of elements you have to delete from the numbers you took (starting from the first one). Last number is the number we search in our collection after the manipulations. If it is present print: “YES!”, otherwise print “NO!”
+
+        // Example: 
+        //              Input                         Output
+        //12 412 123 21 654 34 65 3 23 7 4 21          NO!
+        #endregion
+    }
+}
