186fdbc64fc239f0e81128962a2b0f2eacb9118e
Uendy <alex.beshkov98@gmail.com>
2020-12-04 15:03:44 +0200

    Adding: -Q04 (V4) Start // Going to try to use the method given in the exercises, but will do it my way using a dictionary <int index, list<int>sequence> to keep the sequences

---
+++ L05 Lists/L05 List Qs (V3)/L05 List Qs (V3)/Q04 (V4)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        #region
+        // Instructions: Read a list of integers and find the longest increasing subsequence (LIS). If several such exist, print the leftmost
+        // Example:
+        //          input                                   output:
+        //            1                                        1
+        //      7 3 5 8 -1 0 6 7                            3 5 6 7
+        //     1 2 5 3 5 2 4 1	                            1 2 3 5
+        //  0 10 20 30 30 40 1 50 2 3 4 5 6              0 1 2 3 4 5 6
+        //  11 12 13 3 14 4 15 5 6 7 8 7 16 9 8          3 4 5 6 7 8 16
+        //  3 14 5 12 15 7 8 9 11 10 1                   3 5 7 8 9 11
+        #endregion
+
+        // Going to try to use the method given in the exercises, but will do it my way using a dictinary <int index, list<int>sequence> to keep the sequences
+    }
+}
