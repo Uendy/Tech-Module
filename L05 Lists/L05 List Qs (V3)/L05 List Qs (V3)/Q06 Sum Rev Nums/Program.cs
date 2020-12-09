ad8001d1f62235bccf473ad451206fc5a1bf8a38
Uendy <alex.beshkov98@gmail.com>
2020-12-09 18:39:08 +0200

    Update: -Finished Q06

--- L05 Lists/L05 List Qs (V3)/L05 List Qs (V3)/Q06 Sum Rev Nums/Program.cs
+++ L05 Lists/L05 List Qs (V3)/L05 List Qs (V3)/Q06 Sum Rev Nums/Program.cs
@@ -9,8 +9,32 @@ public class Program
         //Write a program that reads sequence of numbers, reverses their digits, and prints their sum.
 
         // Example:
-        //   Input         Output              Comment
+        //   Input         Output             Comment
         // 123 234 12       774         321 + 432 + 21 = 774
         #endregion
+
+        // Reading input:
+        var list = Console.ReadLine().Split(' ').ToList();
+
+        var revList = list.ToList();
+
+        // Cycle and reverse
+        for (int index = 0; index < list.Count(); index++)
+        {
+            string current = list[index];
+            var reverse = reverseNum(current);
+            revList[index] = reverse;
+        }
+
+        // Sum and Print
+        int sum = revList.Select(int.Parse).Sum();
+        Console.WriteLine(sum);
+    }
+
+    public static string reverseNum(string current)
+    {
+        var rev = current.ToCharArray().Reverse();
+        string reversed = string.Join("", rev);
+        return reversed;
     }
 }
