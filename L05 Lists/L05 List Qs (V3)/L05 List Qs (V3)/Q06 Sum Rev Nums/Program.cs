ff772090022f53cc95e9d85456f6b068ba397617
Uendy <alex.beshkov98@gmail.com>
2020-12-09 17:46:59 +0200

    Adding: -Q06 Sum Rev Num (Sum Reversed Numbers) Start

---
+++ L05 Lists/L05 List Qs (V3)/L05 List Qs (V3)/Q06 Sum Rev Nums/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        #region
+        //Write a program that reads sequence of numbers, reverses their digits, and prints their sum.
+
+        // Example:
+        //   Input         Output              Comment
+        // 123 234 12       774         321 + 432 + 21 = 774
+        #endregion
+    }
+}
