02a84f66b91966eca403d2659c2d8ced6ef62a08
Uendy <alex.beshkov98@gmail.com>
2020-12-10 13:21:37 +0200

    Adding: -Q07 Bomb Numbers

---
+++ L05 Lists/L05 List Qs (V3)/L05 List Qs (V3)/Q07 Bomb Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        #region
+        // Instructions: Write a program that reads sequence of numbers and special bomb number with a certain power. Your task is to detonate every occurrence of the special bomb number and according to its power his neighbors from left and right. Detonations are performed from left to right and all detonated numbers disappear. Finally print the sum of the remaining elements in the sequence.
+        // Example:
+        //     Input             Output                                                             Comment
+        // 1 2 2 4 2 2 2 9         12                   Special number is 4 with power 2. After detontaion we left with the sequence [1, 2, 9] with sum 12.
+        //     4 2
+
+        #endregion
+    }
+}
