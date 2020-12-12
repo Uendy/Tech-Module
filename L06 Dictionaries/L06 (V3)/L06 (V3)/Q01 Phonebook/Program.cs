85560211b2bb335beb2d01cc64a3e34364d48491
Uendy <alex.beshkov98@gmail.com>
2020-12-12 12:03:32 +0200

    Adding: - Q01 Phonebook Start

--- L06 Dictionaries/L06 (V3)/L06 (V3)/Q01 Phonebook/Program.cs
+++ L06 Dictionaries/L06 (V3)/L06 (V3)/Q01 Phonebook/Program.cs
@@ -1,15 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Q01_Phonebook
+public class Program
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        #region
+        // Instructions: Write a program that receives some info from the console about people and their phone numbers. Each entry should have just one name and one number (both of them strings). 
+        // On each line, you will receive some of the following commands:
+        // •	A { name} { phone} – adds entry to the phonebook.In case of trying to add a name that is already in the phonebook you should change the existing phone number with the new one provided.
+        // •	S { name} – searches for a contact by given name and prints it in format "{name} -> {number}".In case the contact isn't found, print "Contact {name} does not exist.".
+        // •	END – stop receiving more commands.
+
+        #endregion
+
+        // Initialize dict
+        var phoneBook = new Dictionary<string, string>();
+
+        // Read input commands
+        var input = Console.ReadLine().Split(' ').ToList();
+        string command = input[0];
+
+        while (command != "END")
         {
+            switch (command)
+            {
+                case "A":
+                    //
+                    break;
+
+                case "S":
+                    // 
+                    break
+    
+            }
+            input = Console.ReadLine().Split(' ').ToList();
+            command = input[0];
         }
     }
 }
