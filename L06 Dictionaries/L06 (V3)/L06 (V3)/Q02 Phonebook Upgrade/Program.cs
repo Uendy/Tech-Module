475a3684357c5f41a3504d23273d66ea5a37a7f4
Uendy <alex.beshkov98@gmail.com>
2020-12-12 13:44:00 +0200

    Adding: -Q03 Miner Task

---
+++ L06 Dictionaries/L06 (V3)/L06 (V3)/Q02 Phonebook Upgrade/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        #region
+        // Instuctions: Add functionality to the phonebook from the previous task to print all contacts ordered lexicographically when receive the command “ListAll”.
+
+        // Example: Input                       Output 
+        // A Nakov +359888001122            Gero-> 5559393
+        // A RoYaL(Ivan) 666            Nakov-> + 359888001122
+        // A Gero 5559393                   RoYaL(Ivan)-> 666
+        // A Simo 02 / 987665544        Simo-> 02 / 987665544
+        // ListAll
+        // END 
+        #endregion
+
+        var phoneBook = new Dictionary<string, string>();
+
+        var input = Console.ReadLine().Split(' ').ToList();
+        var command = input[0];
+
+        while (command != "End")
+        {
+            switch (command)
+            {
+                case "A":
+                    string name = input[1];
+                    string number = input[2];
+
+                    phoneBook[name] = number;
+                    break;
+
+                case "S":
+                    string searchedName = input[1];
+
+                    bool containsContact = phoneBook.ContainsKey(searchedName);
+                    if (containsContact)
+                    {
+                        Console.WriteLine($"{searchedName} -> {phoneBook[searchedName]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {searchedName} does not exist.");
+                    }
+                    break;
+
+                case "ListAll":
+                    foreach (var kvp in phoneBook)
+                    {
+                        Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+                    }
+                    break;
+            }
+
+            input = Console.ReadLine().Split(' ').ToList();
+            command = input[0];
+        }
+    }
+}
---
+++ L06 Dictionaries/L06 (V3)/L06 (V3)/Q03 A Miner Task/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+public class Program
+{
+    public static void Main()
+    {
+        #region
+        // Instructions: You are given a sequence of strings, each on a new line. Every odd line on the console is representing a resource (e.g. Gold, Silver, Copper, and so on), and every even – quantity. Your task is to collect the resources and print them each on a new line. 
+        // Print the resources and their quantities in format:
+        // { resource} –> { quantity}
+        // The quantities inputs will be in the range[1 … 2 000 000 000]
+
+        // Example:
+        // Input              Output  
+        // Gold              Gold -> 155
+        //  155              Silver-> 10
+        // Silver            Copper-> 17
+        //  10
+        // Copper
+        //  17
+        // stop 
+        #endregion
+
+        // Initialize Dictionary
+        var resources = new Dictionary<string, int>();
+
+        // Read input and cycle them, while also not trying to read value input after "stop"
+        string resource = Console.ReadLine();
+        while (resource != "stop")
+        {
+            int value = int.Parse(Console.ReadLine());
+
+            // Check for new key and add value accordingly
+            bool newResource = resources.ContainsKey(resource);
+            if (newResource)
+            {
+                resources[resource] += value;
+            }
+            else
+            {
+                resources[resource] = value;
+            }
+
+            resource = Console.ReadLine();
+        }
+
+        // Printing all output
+        foreach (var kvp in resources)
+        {
+            Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+        }
+    }
+}
