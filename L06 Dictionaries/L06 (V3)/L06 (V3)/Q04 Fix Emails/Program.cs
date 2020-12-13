06367670b94d0b91177429818915f3a384308b34
Uendy <alex.beshkov98@gmail.com>
2020-12-13 19:15:08 +0200

    Adding: -Q04 Fix Emails (Start)

---
+++ L06 Dictionaries/L06 (V3)/L06 (V3)/Q04 Fix Emails/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        // Instructions: You are given a sequence of strings, each on a new line, until you receive the “stop” command. The first string is the name of a person. On the second line, you will receive their email. Your task is to collect their names and emails, and remove emails whose domain ends with "us" or "uk" (case insensitive). Print:
+        // { name} – > { email}
+
+        // Example:
+        //     Input             Output
+        //     Ivan
+        // ivanivan @abv.bg
+        //  Petar Ivanov
+
+    }
+}
