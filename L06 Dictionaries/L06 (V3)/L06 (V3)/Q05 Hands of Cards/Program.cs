8368a935fdd2a5d0b494a722471a500e294d0444
Uendy <alex.beshkov98@gmail.com>
2020-12-14 20:06:03 +0200

    Update: -I am almost finished, but it isn't giving the proper values

--- L06 Dictionaries/L06 (V3)/L06 (V3)/Q05 Hands of Cards/Program.cs
+++ L06 Dictionaries/L06 (V3)/L06 (V3)/Q05 Hands of Cards/Program.cs
@@ -14,5 +14,138 @@ public class Program
         //  Finally print out the total value each player has in his hand in the format:
         //  •	{ personName}: { value}
         #endregion
+
+        // Initialize score (key = name, value = hand)
+        var playersAndHands = new Dictionary<string, List<string>>();
+
+        // Reading input
+        string input = Console.ReadLine();
+        while (input != "JOKER")
+        {
+            // Get the hand and add them to the people
+            var hand = input.Split(':').ToArray();
+            string name = hand[0];
+            var hands = hand[1];
+
+            // Get cards
+            var cards = hands.Split(new string[] { ", " }, StringSplitOptions.None).ToList();
+
+            // Add or Update players with new hands
+            bool newPlayer = !playersAndHands.ContainsKey(name);
+            if (newPlayer)
+            {
+                playersAndHands[name] = cards;
+            }
+            else
+            {
+                playersAndHands[name].InsertRange(0, cards);
+            }
+
+            // Continue reading input and cycling
+            input = Console.ReadLine();
+        }
+
+        // remove duplicates and calculate score
+        foreach (var kvp in playersAndHands)
+        {
+            var uniqueCards = kvp.Value.Distinct();
+
+            int score = 0;
+            foreach (var card in uniqueCards)
+            {
+                char power = card.First(); // 2 to A
+                char type = card.Last(); //S = 4, H = 3, D = 2, C = 1
+
+                var currentScore = FindScore(power, type);
+                score += currentScore;
+            }
+
+            Console.WriteLine($"{kvp.Key}: {score}");
+        }
+    }
+    public static int FindScore(char power, char type)
+    {
+        int score = 0;
+
+        switch (power)
+        {
+            case '2':
+                score = 2;
+                break;
+
+            case '3':
+                score = 3;
+                break;
+
+            case '4':
+                score = 4;
+                break;
+
+            case '5':
+                score = 5;
+                break;
+
+            case '6':
+                score = '6';
+                break;
+
+            case '7':
+                score = 7;
+                break;
+
+            case '8':
+                score = 8;
+                break;
+
+            case '9':
+                score = 9;
+                break;
+
+            case '1':           // No need for 0 as there is no '1' card --> its a 10
+                score = 10;
+                break;
+
+            case 'J':
+                score = 11;
+                break;
+
+            case 'Q':
+                score = 12;
+                break;
+
+            case 'K':
+                score = 13;
+                break;
+
+            case 'A':
+                score = 14;
+                break;
+
+            default:
+                break;
+        }
+        switch (type)
+        {
+            case 'S':
+                score *= 4;
+                break;
+
+            case 'H':
+                score *= 3;
+                break;
+
+            case 'D':
+                score *= 2;
+                break;
+
+            case 'C':
+                score *= 1;
+                break;
+
+            default:
+                break;
+        }
+
+        return score;
     }
 }
